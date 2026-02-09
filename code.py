177bc89f3e842672e1b2d9e817a40399e332f429
Analyzer <analyzer@test.com>
2026-02-09 13:26:55 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,4 @@
-from collections import Counter
-
-k = int(input())
 s = input().strip()
-
-total_length = len(s)
-if total_length % k != 0:
-    print(-1)
-else:
-    cnt = Counter(s)
-    possible = True
-    for c, count in cnt.items():
-        if count % k != 0:
-            possible = False
-            break
-    if not possible:
-        print(-1)
-    else:
-        base = []
-        for char in sorted(cnt.keys()):
-            base.append(char * (cnt[char] // k))
-        base_str = ''.join(base)
-        result = base_st
+parts = s.split('+')
+parts.sort()
+print('+'.join(parts))
