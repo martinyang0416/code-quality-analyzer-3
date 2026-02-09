c5b9fee570026b6e54f7bd281a17c53d1c82c06d
Analyzer <analyzer@test.com>
2026-02-09 14:53:38 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,11 @@
-from functools import lru_cache
-
-class Solution:
-    def minDays(self, n: int) -> int:
-        @lru_cache(maxsize=None)
-        def dfs(remaining):
-            if remaining <= 1:
-                return remaining
-            option1 = 1 + dfs(remaining - 1)
-            option2 = (remaining % 2) + 1 + dfs(remaining // 2)
-            option3 = (remaining % 3) + 1 + dfs(remaining // 3)
-            return min(option1, option2, option3)
-        
-        return dfs(n)
+def consecutiveNumbersSum(N):
+    count = 0
+    k = 1
+    while True:
+        temp = N - k * (k - 1) // 2
+        if temp <= 0:
+            break
+        if temp % k == 0:
+            count += 1
+        k += 1
+    return count
