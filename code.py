a4b6823a735199383e6e9b2c3508fed68015bc99
Analyzer <analyzer@test.com>
2026-02-09 10:06:07 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,24 @@
-n = int(input())
-a = list(map(int, input().split()))
-up = [1] * n
-down = [1] * n
-
-for i in range(n):
-    for j in range(i):
-        if a[j] < a[i]:
-            if down[j] + 1 > up[i]:
-                up[i] = down[j] + 1
-        elif a[j] > a[i]:
-            if up[j] + 1 > down[i]:
-                down[i] = up[j] + 1
-
-max_length = max(max(up), max(down))
-print(max_length)
+def main():
+    import sys
+    input = sys.stdin.read().split()
+    idx = 0
+    M = int(input[idx])
+    idx += 1
+    D = int(input[idx])
+    idx += 1
+    
+    stations = []
+    for _ in range(M):
+        f = int(input[idx])
+        p = int(input[idx + 1])
+        stations.append((p, f))
+        idx += 2
+    
+    stations.sort()  # Sort by position
+    left = 0
+    current_sum = 0
+    max_sum = 0
+    
+    for right in range(len(stations)):
+        current_sum += stations[right][1]
+        while s
