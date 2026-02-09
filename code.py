9c59ecaa8714a2b3e0161a2384dc18a4cc31d516
Analyzer <analyzer@test.com>
2026-02-09 13:53:52 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,19 @@
+import sys
+
 def main():
-    import sys
-    S, T, q = sys.stdin.readline().split()
-    q = int(q)
-    n = len(S)
-    m = len(T)
+    n = int(sys.stdin.readline())
+    objects = []
+    for _ in range(n):
+        parent, typ = map(int, sys.stdin.readline().split())
+        objects.append((parent, typ))
     
-    # Precompute the merged strings' comparison keys
-    # But we can't store all merged strings; compare on the fly
-    
-    for _ in range(q):
-        l, r, k, x, y = map(int, sys.stdin.readline().split())
-        candidates = []
-        # Generate all i in [l, r] where i mod k is in [x, y]
-        # This can be optimized by finding the first i >= l with i mod k = c, then ste
+    # Precompute type0_parent and part_leader
+    type0_parent = [-1] * (n + 1)
+    part_leader = [0] * (n + 1)
+    for i in range(1, n + 1):
+        parent, typ = objects[i-1]
+        if parent != -1:
+            if typ == 0:
+                type0_parent[i] = parent
+            else:
+            
