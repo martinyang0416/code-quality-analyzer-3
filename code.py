9fd4b19efd2c73618d38763b2f4a753c2c5000f7
Analyzer <analyzer@test.com>
2026-02-09 13:55:10 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,21 @@
-n = int(input())
+import sys
 
-if n == 1:
-    print(2)
-else:
-    primes = [2]
-    candidate = 3
-    while len(primes) < n:
-        is_prime = True
-        sqrt_m = int(candidate ** 0.5) + 1
-        for p in primes:
-            if p > sqrt_m:
-                break
-            if candidate % p == 0:
-                is_prime = False
-                break
-        if is_prime:
-            primes.append(candidate)
-        candidate += 2
-    print(primes[-1])
+max_n = 10**6
+
+# Precompute smallest prime factors (SPF)
+spf = list(range(max_n + 1))
+for i in range(2, int(max_n**0.5) + 1):
+    if spf[i] == i:
+        for j in range(i*i, max_n + 1, i):
+            if spf[j] == j:
+                spf[j] = i
+
+# Precompute results for all possible n
+precomputed = [""] * (max_n + 1)
+for n in range(2, max_n + 1):
+    factors = {}
+    temp = n
+    while temp > 1:
+        p = spf[temp]
+        while temp % p == 0:
+            factors[p] = factors.get(p,
