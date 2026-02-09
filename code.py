87d96f666d9655bb320a0216b1c22dae289d9411
Analyzer <analyzer@test.com>
2026-02-09 10:05:10 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,13 @@
-def main():
-    import sys
-    input = sys.stdin.read().split()
-    idx = 0
-    N, K = int(input[idx]), int(input[idx+1])
-    idx += 2
-    
-    cuboids = []
-    x_coords = set()
-    y_coords = set()
-    z_coords = set()
-    
-    for _ in range(N):
-        x1 = int(input[idx])
-        y1 = int(input[idx+1])
-        z1 = int(input[idx+2])
-        x2 = int(input[idx+3])
-        y2 = int(input[idx+4])
-        z2 = int(input[idx+5])
-        idx += 6
-        
-        cuboids.append( (x1, y1, z1, x2, y
+n, x = map(int, input().split())
+k = int(input())
+c = 0
+for _ in range(k):
+    t, _ = map(int, input().split())
+    if t == 1:
+        c += 1
+
+u = (x - 1) - k
+min_skipped = 0
+max_skipped = u
+
+print(min_skipped, max_skipped)
