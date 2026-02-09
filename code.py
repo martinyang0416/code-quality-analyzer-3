7e395137a0138cec0e7281140d3706eb8c517476
Analyzer <analyzer@test.com>
2026-02-09 10:23:50 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,23 @@
-import sys
+import math
 
-def main():
-    input = sys.stdin.read().split()
-    t = int(input[0])
-    idx = 1
-    for _ in range(t):
-        m = int(input[idx])
-        idx += 1
-        if m == 2:
-            print(1)
-            print(2)
-        else:
-            k = m - 1
-            print(k)
-            print('1 ' * (m - 2) + '2')
-
-if __name__ == "__main__":
-    main()
+def minimal_additional_matches():
+    n = int(input())
+    for _ in range(n):
+        w, t, a, b = map(int, input().split())
+        if a == 0:
+            if w == 0:
+                print(0)
+            else:
+                print(-1)
+            continue
+        
+        g = math.gcd(a, b)
+        a_prime = a // g
+        b_prime = b // g
+        
+        if a_prime > b_prime:
+            print(-1)
+            continue
+        
+        if a_prime == b_prime:
+            if w == t:
