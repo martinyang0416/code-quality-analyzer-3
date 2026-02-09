d29b9f6e600def089a0396f7d152128bad079e29
Analyzer <analyzer@test.com>
2026-02-09 14:19:59 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,19 @@
-def trap(height):
-    if not height:
-        return 0
-    left = 0
-    right = len(height) - 1
-    left_max = right_max = result = 0
-    while left <= right:
-        if left_max <= right_max:
-            if height[left] > left_max:
-                left_max = height[left]
-            else:
-                result += left_max - height[left]
-            left += 1
-        else:
-            if height[right] > right_max:
-                right_max = height[right]
-            else:
-                result
+from typing import List
+
+class Solution:
+    def minOperationsMaxProfit(self, customers: List[int], boardingCost: int, runningCost: int) -> int:
+        waiting = 0
+        boarded = 0
+        rotations = 0
+        max_profit = -float('inf')
+        best_rotations = -1
+        i = 0
+        n = len(customers)
+        
+        while i < n or waiting > 0:
+            if i < n:
+                waiting += customers[i]
+                i += 1
+            
+            boarding = min(4, waiting)
+       
