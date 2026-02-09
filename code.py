8615c6f339cc7863e8b97195747700442f260db1
Analyzer <analyzer@test.com>
2026-02-09 14:18:37 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,16 @@
-def integerBreak(n):
-    if n == 2:
-        return 1
-    if n == 3:
-        return 2
-    quotient, remainder = divmod(n, 3)
-    if remainder == 0:
-        return 3 ** quotient
-    elif remainder == 1:
-        return 3 ** (quotient - 1) * 4
-    else:
-        return 3 ** quotient * 2
+def longest_absolute_path(s):
+    max_len = 0
+    stack = []
+    for line in s.split('\n'):
+        depth = line.count('\t')
+        name = line[depth:]
+        while len(stack) > depth:
+            stack.pop()
+        current_length = len(name)
+        if stack:
+            current_length += stack[-1] + 1  # add parent's length and slash
+        if '.' in name:
+            max_len = max(max_len, current_length)
+        else:
+            stack.append(current_length)
+    return max_len
