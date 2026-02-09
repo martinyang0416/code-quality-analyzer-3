27ed7f05729e21ef2f23a1ab8c5a51ef1339703a
Analyzer <analyzer@test.com>
2026-02-09 14:39:45 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,12 @@
-def minCost(s, cost):
-    total = 0
-    prev_char = s[0]
-    sum_group = cost[0]
-    max_group = cost[0]
-    
-    for i in range(1, len(s)):
-        if s[i] == prev_char:
-            sum_group += cost[i]
-            if cost[i] > max_group:
-                max_group = cost[i]
+def simplifyPath(path: str) -> str:
+    stack = []
+    parts = path.split('/')
+    for part in parts:
+        if part == '..':
+            if stack:
+                stack.pop()
+        elif part == '.' or not part:
+            continue
         else:
-            total += sum_group - max_group
-            prev_char = s[i]
-            sum_group = cost[i]
-            max_group = cost[i]
-    
-    total += sum_group - max_group
-    return total
+            stack.append(part)
+    return '/' + '/'.join(stack) if stack else '/'
