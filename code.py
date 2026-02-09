ea1e3eca78c264b28195accbb5783ccb5f1b770d
Analyzer <analyzer@test.com>
2026-02-09 14:44:21 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,18 @@
-class DSU:
-    def __init__(self):
-        self.parent = {}
-        self.size = {}
+def maximumGap(nums):
+    n = len(nums)
+    if n < 2:
+        return 0
+    min_val = min(nums)
+    max_val = max(nums)
+    if min_val == max_val:
+        return 0
+    bucket_count = n - 1
+    buckets = [{'min': None, 'max': None} for _ in range(bucket_count)]
     
-    def find(self, x):
-        if x not in self.parent:
-            self.parent[x] = x
-            self.size[x] = 1
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-    
-    def union(self, x, y):
-        x_root = self.find(x)
-        y_root = self.find(y)
-        if x_root == y_root:
-            return
-        if self.size[x_root] < self.siz
+    for x in nums:
+        if x == max_val:
+            bucket_idx = bucket_count - 1
+        else:
+            bucket_idx = ((x - min_val) * bucket_count) // (max_val - min_val)
+        if bucket_idx >= bucket_count:
+            bucke
