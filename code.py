09980635a22293ff17aae90d9de38b8efdb180d5
Analyzer <analyzer@test.com>
2026-02-09 13:38:54 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,10 @@
-import sys
-
-rect_count = 0
-rhombus_count = 0
-
-for line in sys.stdin:
-    line = line.strip()
-    if not line:
-        continue
-    a, b, c = map(int, line.split(','))
-    if a**2 + b**2 == c**2:
-        rect_count += 1
-    if a == b:
-        rhombus_count += 1
-
-print(rect_count)
-print(rhombus_count)
+s = input().strip()
+a = c = m = 0
+for char in s:
+    if char == 'A':
+        a += 1
+    elif char == 'C':
+        c += a
+    elif char == 'M':
+        m += c
+print(m)
