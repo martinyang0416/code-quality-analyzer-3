ec0925d76aea126635d0eb8262927b4e4808b59b
Analyzer <analyzer@test.com>
2026-02-09 15:02:26 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,24 @@
-def arrange_words(text: str) -> str:
-    words = text.split()
-    if not words:
-        return ""
-    indexed_words = [(len(word), idx, word) for idx, word in enumerate(words)]
-    sorted_words = [word for _, _, word in sorted(indexed_words, key=lambda x: (x[0], x[1]))]
-    processed = [sorted_words[0].capitalize()]
-    for word in sorted_words[1:]:
-        processed.append(word.lower())
-    return ' '.join(processed)
+import heapq
+
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    idx = 0
+    N = int(data[idx])
+    Q = int(data[idx+1])
+    idx += 2
+    roadworks = []
+    for _ in range(N):
+        S = int(data[idx])
+        T = int(data[idx+1])
+        X = int(data[idx+2])
+        idx +=3
+        lower = S - X - 0.5
+        upper = T - X - 0.5
+        roadworks.append( (lower, upper, X) )
+    roadworks.sort()
+    ds = list(map(int, data[idx:idx+Q]))
+    heap = []
+    j = 0
+ 
