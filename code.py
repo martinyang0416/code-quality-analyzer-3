68c7bcca9678ffc083f47ef13b6ef67296faa521
Analyzer <analyzer@test.com>
2026-02-09 10:13:43 -0800

    Update

--- code.py
+++ code.py
@@ -1,26 +1,23 @@
-import sys
-sys.setrecursionlimit(1 << 25)
-
-def main():
+def minimal_cost():
     import sys
     input = sys.stdin.read
     data = input().split()
-    idx = 0
-    n = int(data[idx])
-    idx += 1
-    m = int(data[idx])
-    idx += 1
-
-    parent = [i for i in range(n + 1)]  # 1-based indexing
-    rank = [1] * (n + 1)
-
-    def find(u):
-        while parent[u] != u:
-            parent[u] = parent[parent[u]]
-            u = parent[u]
-        return u
-
-    def union(u, v):
-        u_root = find(u)
-        v_root = find(v)
-  
+    
+    t = int(data[0])
+    idx = 1
+    for _ in range(t):
+        n = int(data[idx])
+        idx += 1
+        c = list(map(int, data[idx:idx+n]))
+        idx += n
+        
+        prefix = [0] * (n + 1)
+        for i in range(1, n+1):
+            prefix[i] = prefix[i-1] + c[i-1]
+        
+        if n == 0:
+            print(0)
+            continue
+        
+        right_dp = [0] * (n + 1)
+        min_righ
