9d9cb93b95fdc64646d1ad02ad479c36e5c20d94
Analyzer <analyzer@test.com>
2026-02-09 15:03:55 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,23 @@
-import bisect
-import sys
-
 def main():
-    sys.setrecursionlimit(1 << 25)
-    N = int(sys.stdin.readline())
-    a = list(map(int, sys.stdin.readline().split()))
-    adj = [[] for _ in range(N+1)]
-    for _ in range(N-1):
-        u, v = map(int, sys.stdin.readline().split())
-        adj[u].append(v)
-        adj[v].append(u)
-    ans = [0] * (N + 1)
-    tails = []
-    stack = []
-    change_stack = []
-    stack.append((1, None, False))
-    while stack:
-        u, parent, visited = stack.pop()
-       
+    import sys
+    data = list(map(int, sys.stdin.readline().split()))
+    ptr = 0
+    N = data[ptr]
+    ptr += 1
+    k = data[ptr]
+    ptr += 1
+    V = data[ptr:ptr+N]
+    ptr += N
+    B = data[ptr:ptr+N]
+    
+    dp = {tuple(): 0}
+    for i in range(N):
+        b = B[i]
+        v = V[i]
+        new_dp = {}
+        for stack in dp:
+            current_sum = dp[stack]
+            # Option 1: do not take
+            if stack in new_dp:
+                if current_sum > new_dp[stack]:
+ 
