33c364f9844d78f87cf78c6deda594d5c245789e
Analyzer <analyzer@test.com>
2026-02-09 12:15:41 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,19 @@
-def fact(i):
-  ans = 1
-  for j in range(1, i + 1):
-    ans *= j
-  return ans
+import sys
+import time
+import itertools
+from itertools import accumulate, product, permutations, combinations
+import collections
+from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
+from functools import lru_cache
+import math
+from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
+import fractions
+from typing import List, Tuple
+import numpy as np
+import random
+import heapq
+from heapq import *
+from dataclasses import dataclass
 
-def c(i, j):
-  return fact(i) // (fact(j) * fact(i - j))
-
-n = int(input())
-ans = 1
-for j in range(5):
- ans *= n - j
-print(ans * c(n, 5))
+import builtins
+import re
