70bd606cf0eabe93495106862a2c203649588aec
Analyzer <analyzer@test.com>
2026-02-09 14:14:45 -0800

    Update

--- code.py
+++ code.py
@@ -1,5 +1,17 @@
-def minimumOneBitOperations(n):
-    if n == 0:
-        return 0
-    k = n.bit_length() - 1
-    return (1 << (k + 1)) - 1 - minimumOneBitOperations(n - (1 << k))
+def findLUSlength(strs):
+    def is_subsequence(s, t):
+        it = iter(t)
+        return all(c in it for c in s)
+    
+    max_length = -1
+    for i in range(len(strs)):
+        is_candidate = True
+        for j in range(len(strs)):
+            if i == j:
+                continue
+            if is_subsequence(strs[i], strs[j]):
+                is_candidate = False
+                break
+        if is_candidate:
+            current_length = len(strs[i])
+            if current_length > max_length:
