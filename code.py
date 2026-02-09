3cadaa285404cf53ff6e2f62e754694706c132d3
Analyzer <analyzer@test.com>
2026-02-09 14:17:59 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,12 @@
-def minNumber(num, k):
-    def count_inversions(s):
-        freq = [0] * 10
-        inversions = 0
-        for c in reversed(s):
-            d = int(c)
-            for i in range(d):
-                inversions += freq[i]
-            freq[d] += 1
-        return inversions
-    
-    inversions = count_inversions(num)
-    if k >= inversions:
-        return ''.join(sorted(num))
-    
-    num_list = list(num)
-    n = len(num_list)
-    for i in range(n):
-        if k <= 0:
-            break
-        j_ma
+def integerBreak(n):
+    if n == 2:
+        return 1
+    if n == 3:
+        return 2
+    quotient, remainder = divmod(n, 3)
+    if remainder == 0:
+        return 3 ** quotient
+    elif remainder == 1:
+        return 3 ** (quotient - 1) * 4
+    else:
+        return 3 ** quotient * 2
