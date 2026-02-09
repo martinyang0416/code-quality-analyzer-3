eaeed83bad3bf1827b6209a0685cbe1671af9a85
Analyzer <analyzer@test.com>
2026-02-09 10:33:13 -0800

    Update

--- code.py
+++ code.py
@@ -1,25 +1,24 @@
-n, q = map(int, input().split())
+import sys
 
-lower = [0] * (n + 1)
-upper = [0] * (n + 1)
-
-for i in range(1, n + 1):
-    lower[i] = 1
-    upper[i] = n
-
-for _ in range(q):
-    t, l, r, v = map(int, input().split())
-    if t == 1:
-        for x in range(l, r + 1):
-            if lower[x] < v:
-                lower[x] = v
-    else:
-        for x in range(l, r + 1):
-            if upper[x] > v:
-                upper[x] = v
-
-valid = True
-total = 0
-for x in range(1, n + 1):
-    if lower[x] > upper[x]:
-        val
+def main():
+    n, m, k = map(int, sys.stdin.readline().split())
+    d = list(map(int, sys.stdin.readline().split()))
+    
+    start_count = 0
+    start_node = -1
+    max_d = 0
+    for i in range(n):
+        dist = d[i]
+        if dist == 0:
+            start_count += 1
+            start_node = i + 1
+        if dist > max_d:
+            max_d = dist
+    
+    if start_count != 1:
+        print(-1)
+        return
+    
+    # Check all distances 1 to max_d exist
+    possible = True
+    f
