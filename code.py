3912d55a3d8078d3188308308411d00b539633df
Analyzer <analyzer@test.com>
2026-02-09 13:05:50 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,24 @@
-n = int(input())
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-c = list(map(int, input().split()))
+import bisect
+import math
+from collections import defaultdict
 
-error1 = sum(a) - sum(b)
-error2 = sum(b) - sum(c)
+def reverse_num(x):
+    return int(str(x)[::-1].lstrip('0') or '0')
 
-print(error1)
-print(error2)
+maxx, maxy, w = map(int, input().split())
+
+# Preprocess a_ratios and b_ratios
+a_ratios = defaultdict(list)
+for a in range(1, maxx + 1):
+    rev_a = reverse_num(a)
+    d = math.gcd(a, rev_a)
+    p = a // d
+    q = rev_a // d
+    a_ratios[(p, q)].append(a)
+
+for key in a_ratios:
+    a_ratios[key].sort()
+
+b_ratios = defaultdict(list)
+for b in range(1, maxy + 1):
+    rev
