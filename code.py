1c6961bbbd946a5598e053d444332b4114013c38
Analyzer <analyzer@test.com>
2026-02-09 13:01:54 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,14 @@
-import sys
-import time
-import itertools
-from itertools import accumulate, product, permutations, combinations
-import collections
-from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
-from functools import lru_cache
-import math
-from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
-import fractions
-from typing import List, Tuple
-import numpy as np
-import random
-import heapq
-from heapq import *
-from dataclasses import dataclass
+import bisect
 
-import builtins
-import re
+def count_balanced_substrings(s):
+    n = len(s)
+    count_4 = [0] * (n + 1)
+    for i in range(1, n+1):
+        count_4[i] = count_4[i-1] + (1 if s[i-1] == '4' else 0)
+    ans = 0
+    for L in range(1, n+1):
+        a = count_4[L-1]
+        # Binary search for the smallest R >= L where count_4[R] > a
+        # The count_4 array is non-decreasing, so we can use bisect
+        # We need to search in the subarray count_4[L..n]
+        # Using bisect_right to find the first element >
