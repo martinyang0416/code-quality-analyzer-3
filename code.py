90df13366ce70bb82836378bb3ad654486785de5
Analyzer <analyzer@test.com>
2026-02-09 15:15:58 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,19 @@
-import sys
+MOD = 10**9 + 7
+max_pow = 10**5  # Since n can be up to 1e5, n-L can be up to 1e5 -1
+pow26 = [1] * (max_pow + 1)
 
-data = list(map(int, sys.stdin.read().split()))
-ptr = 0
-T = data[ptr]
-ptr += 1
-for _ in range(T):
-    N = data[ptr]
-    ptr += 1
-    max_h = max(data[ptr:ptr+N])
-    ptr += N
-    print(max_h)
+for i in range(1, max_pow + 1):
+    pow26[i] = (pow26[i-1] * 26) % MOD
+
+T = int(input())
+for case in range(1, T+1):
+    n, m = map(int, input().split())
+    patterns = [input().strip() for _ in range(m)]
+    print(f"Case {case}:")
+    for s in patterns:
+        L = len(s)
+        if L > n:
+            print(0)
+        else:
+            e = n - L
+            res = ((n - L + 1) * pow26
