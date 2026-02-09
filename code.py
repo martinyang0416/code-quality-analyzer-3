e555130a8ef6a8f40412043619f4f1285e5db812
Analyzer <analyzer@test.com>
2026-02-09 13:58:40 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,19 @@
-import sys
-import math
+def compute_sum(a, b, c):
+    if b < c:
+        n = b - a + 1
+        first = c - b
+        last = c - a
+        return (first + last) * n // 2
+    elif a > c:
+        n = b - a + 1
+        first = a - c
+        last = b - c
+        return (first + last) * n // 2
+    else:
+        left_part = c - a
+        left_sum = left_part * (left_part + 1) // 2
+        right_part = b - c
+        right_sum = right_part * (right_part + 1) // 2
+        return left_sum + right_sum
 
-def sieve(n):
-    sieve = [True] * (n + 1)
-    sieve[0] = sieve[1] = False
-    for i in range(2, int(math.isqrt(n)) + 1):
-        if sieve[i]:
-            sieve[i*i::i] = [False] * len(sieve[i*i::i])
-    primes = [i for i, is_p in enumerate(sieve) if is_p]
-    return primes
-
-base_primes = sieve(31623)
-
-t = int(sys.stdin.readline())
-for case in range(t):
-    m, n = map(int, sys.stdin.readline().split())
-    size = n - m + 1
-    is_prime = [True] * size
-    
-    for i in ra
+n, K = map(int, input().split
