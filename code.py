df867b9c34232179a694a201bb683dd3b98be61e
Analyzer <analyzer@test.com>
2026-02-09 10:14:19 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,22 @@
-def minimal_cost():
+def minimal_games():
     import sys
     input = sys.stdin.read
     data = input().split()
-    
     t = int(data[0])
-    idx = 1
+    index = 1
     for _ in range(t):
-        n = int(data[idx])
-        idx += 1
-        c = list(map(int, data[idx:idx+n]))
-        idx += n
+        w = int(data[index])
+        l = int(data[index+1])
+        p = int(data[index+2])
+        q = int(data[index+3])
+        index +=4
         
-        prefix = [0] * (n + 1)
-        for i in range(1, n+1):
-            prefix[i] = prefix[i-1] + c[i-1]
-        
-        if n == 0:
-            print(0)
+        if p == q:
+            # Desired rate is 1
+            if l == 0:
+                print(0)
+            else:
+                print(-1)
             continue
-        
-        right_dp = [0] * (n + 1)
-        min_righ
+        if p == 0:
+            if q != 
