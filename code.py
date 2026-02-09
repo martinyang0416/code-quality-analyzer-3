0449903157f78a98c0ff5ccbb6b67a4c0b9c34c7
Analyzer <analyzer@test.com>
2026-02-09 13:26:25 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,23 @@
-import sys
-from itertools import combinations
+from collections import Counter
 
-def main():
-    input = sys.stdin.read().split()
-    ptr = 0
-    t = int(input[ptr])
-    ptr += 1
-    for _ in range(t):
-        n, c, q = map(int, input[ptr:ptr+3])
-        ptr +=3
-        p = list(map(int, input[ptr:ptr+n]))
-        ptr +=n
-        queries = []
-        for __ in range(q):
-            i, j = map(int, input[ptr:ptr+2])
-            ptr +=2
-            queries.append( (i-1, j) )  # converting to 0-based index
-        
-        # Genera
+k = int(input())
+s = input().strip()
+
+total_length = len(s)
+if total_length % k != 0:
+    print(-1)
+else:
+    cnt = Counter(s)
+    possible = True
+    for c, count in cnt.items():
+        if count % k != 0:
+            possible = False
+            break
+    if not possible:
+        print(-1)
+    else:
+        base = []
+        for char in sorted(cnt.keys()):
+            base.append(char * (cnt[char] // k))
+        base_str = ''.join(base)
+        result = base_st
