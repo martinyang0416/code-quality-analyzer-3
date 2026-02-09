34aeab416953111fc73f57c4569ead59761d073b
Analyzer <analyzer@test.com>
2026-02-09 10:08:44 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,8 @@
-n = int(input())
-scores = list(map(int, input().split()))
-scores.sort()
-
-max_rating = 0
-for R in range(n, 0, -1):
-    # Binary search to find the first index where score >= R
-    left, right = 0, n
-    while left < right:
-        mid = (left + right) // 2
-        if scores[mid] >= R:
-            right = mid
-        else:
-            left = mid + 1
-    count = n - left
-    if count >= R:
-        max_rating = R
-        break
-
-print(max_rating)
+x, y, z = map(int, input().split())
+min_profit = min(x, y, z)
+if x == min_profit:
+    print("Apple")
+elif y == min_profit:
+    print("Banana")
+else:
+    print("Cherry")
