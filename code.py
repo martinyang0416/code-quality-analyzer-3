d600684ee85f6b4a74098c260e1362c583b13f93
Analyzer <analyzer@test.com>
2026-02-09 15:20:00 -0800

    Update

--- code.py
+++ code.py
@@ -1,7 +1,16 @@
+# Read the number of test cases
 T = int(input())
 for _ in range(T):
-    K = int(input())
-    for i in range(1, K+1):
-        start = (i-1) * K + 1
-        line = ''.join(str(start + j) for j in range(K))
-        print(line)
+    # Read N and S for each test case
+    N, S = map(int, input().split())
+    # Read the list of prices
+    P = list(map(int, input().split()))
+    # Read the list of types (0 for defender, 1 for forward)
+    types = list(map(int, input().split()))
+    
+    # Separate the players into defenders and forwards based on their type
+    defenders = []
+    forwards = []
+    for price, t in zip(P, types):
+        if t == 0:
+           
