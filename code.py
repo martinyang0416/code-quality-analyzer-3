75a43661031b1ec5a373e9f18b5da8e04769a1a7
Analyzer <analyzer@test.com>
2026-02-09 14:50:00 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,22 @@
-def wordBreak(s, wordDict):
-    word_set = set(wordDict)
-    max_length = max(len(word) for word in word_set) if word_set else 0
-    n = len(s)
-    dp = [False] * (n + 1)
-    dp[0] = True  # Empty string can be segmented
-    
-    for i in range(1, n + 1):
-        start = max(0, i - max_length)
-        for j in range(start, i):
-            if dp[j] and s[j:i] in word_set:
-                dp[i] = True
-                break  # No need to check further once found
-    return dp[n]
+def catMouseGame(graph):
+    memo = {}
+
+    def dfs(m, c, turn):
+        key = (m, c, turn)
+        if key in memo:
+            return memo[key]
+        
+        if m == 0:
+            memo[key] = 1
+            return 1
+        if m == c:
+            memo[key] = 2
+            return 2
+        
+        if turn == 1:
+            next_positions = [pos for pos in graph[c] if pos != 0]
+            if not next_positions:
+                memo[key] = 0
+                return 0
+        
+        memo[key]
