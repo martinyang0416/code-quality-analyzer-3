4f5715d009667ea1de08769880fb47085a9596bf
Analyzer <analyzer@test.com>
2026-02-09 10:38:23 -0800

    Update

--- code.py
+++ code.py
@@ -1,3 +1,13 @@
-n = int(input())
-result = (n // 2) ** 2
-print(result)
+def find_mth_number(M):
+    if M == 0:
+        return 0
+    digits = []
+    while M > 0:
+        rem = M % 7
+        digits.append(str(rem))
+        M = M // 7
+    digits.reverse()
+    return int(''.join(digits))
+
+M = int(input())
+print(find_mth_number(M))
