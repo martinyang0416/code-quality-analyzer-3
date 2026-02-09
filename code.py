a4527d4b310c37cc5711e38375aa5659d3a4f989
Analyzer <analyzer@test.com>
2026-02-09 14:22:58 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,14 @@
-def longestMountain(A):
-    max_len = 0
-    up = 0
-    down = 0
-    n = len(A)
-    if n < 3:
-        return 0
-    for i in range(1, n):
-        if A[i] > A[i-1]:
-            if down > 0:
-                up = 0
-                down = 0
-            up += 1
-        elif A[i] < A[i-1]:
-            if up > 0:
-                down += 1
-                current = up + down + 1
-                if current > max_len:
-                    max_len = current
-            else:
-                up = 0
-           
+from functools import cmp_to_key
+
+def largestNumber(nums):
+    strs = list(map(str, nums))
+    
+    def compare(a, b):
+        if a + b > b + a:
+            return -1
+        else:
+            return 1
+    
+    strs.sort(key=cmp_to_key(compare))
+    result = ''.join(strs)
+    return '0' if result[0] == '0' else result
