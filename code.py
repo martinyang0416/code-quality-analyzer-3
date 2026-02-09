2c60785e77119aa4135fd81d29aa234f0182be0e
Analyzer <analyzer@test.com>
2026-02-09 11:25:45 -0800

    Update

--- code.py
+++ code.py
@@ -1,24 +1,23 @@
-import bisect
+def ceildiv(a, b):
+    if b == 0:
+        return 0 if a == 0 else float('inf')
+    return (a + b - 1) // b
 
 def putaway(A, B, T, X, Y, W, S):
-    sorted_X = sorted(X)
-    sorted_Y = sorted(Y)
+    eligible_weak = [False] * T
+    eligible_small = [False] * T
 
-    only_weak = 0
-    only_small = 0
-    both = 0
-    weak_eligible = []
-    small_eligible = []
-
-    for w, s in zip(W, S):
-        can_weak = False
-        can_small = False
-
+    for i in range(T):
+        w = W[i]
+        s_val = S[i]
+        ew = False
         if A > 0:
-            pos = bisect.bisect_right(sorted_X, w)
-            if pos < A:
-                can_weak = True
+            for x in X:
+                if x > w:
+                    ew = True
+                    break
+        eligible_weak[i] = ew
+
+        es = False
         if B > 0:
-            pos = bisect.bisect_right(sorted_Y, s)
-            if pos < B:
-     
+            for y in
