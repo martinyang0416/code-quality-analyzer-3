8c2c4ea2ade8efd8e0b97d5f9ce21aa9ef6d068c
Analyzer <analyzer@test.com>
2026-02-09 11:15:04 -0800

    Update

--- code.py
+++ code.py
@@ -1,3 +1,14 @@
-a, b = map(int, input().split())
-reversed_b = int(str(b)[::-1])
-print(a + reversed_b)
+a1, a2, a3 = map(int, input().split())
+n = a3 + 1
+
+if n == 1:
+    print(a1)
+elif n == 2:
+    print(a2)
+else:
+    prev_prev = a1
+    prev = a2
+    for i in range(3, n + 1):
+        current = prev_prev + prev
+        prev_prev, prev = prev, current
+    print(prev)
