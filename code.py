8451bf28873e4d217891c43b6b696cf53c943c26
Analyzer <analyzer@test.com>
2026-02-09 14:12:45 -0800

    Update

--- code.py
+++ code.py
@@ -1,25 +1,7 @@
-import sys
-
-class State:
-    __slots__ = ['len', 'link', 'next']
-    def __init__(self):
-        self.len = 0
-        self.link = -1
-        self.next = dict()
-
-def main():
-    input = sys.stdin.read().split()
-    T = int(input[0])
-    cases = input[1:T+1]
-    for s in cases:
-        sa = [State()]
-        sa[0].link = -1
-        sa[0].len = 0
-        last = 0
-        size = 1
-        cnt = [0]
-        for c in s:
-            p = last
-            curr = size
-            sa.append(State())
-      
+def superPow(a, b):
+    mod = 1337
+    a_mod = a % mod
+    result = 1
+    for digit in b:
+        result = (pow(result, 10, mod) * pow(a_mod, digit, mod)) % mod
+    return result
