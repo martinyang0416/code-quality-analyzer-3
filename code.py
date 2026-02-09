0b2ede15c5a2cc6e7d0822abd67836344c4be655
Analyzer <analyzer@test.com>
2026-02-09 14:30:11 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,22 @@
-def wiggleMaxLength(nums):
-    if not nums:
+def numberOfGoodSplits(s):
+    n = len(s)
+    if n < 2:
         return 0
-    up = down = 1
-    for i in range(1, len(nums)):
-        if nums[i] > nums[i-1]:
-            up = down + 1
-        elif nums[i] < nums[i-1]:
-            down = up + 1
-    return max(up, down)
+    
+    # Compute left array using frequency counts
+    left = []
+    freq = [0] * 26
+    distinct = 0
+    for char in s:
+        idx = ord(char) - ord('a')
+        if freq[idx] == 0:
+            distinct += 1
+        freq[idx] += 1
+        left.append(distinct)
+    
+    # Compute right array using frequency counts
+    right = [0] * n
+    freq_right = [0] * 26
+    distinct_right = 0
+    for i in reversed(range(n)):
+       
