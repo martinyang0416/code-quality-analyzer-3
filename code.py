e0dffb623805b5320904b348c712a28a01053442
Analyzer <analyzer@test.com>
2026-02-09 15:11:38 -0800

    Update

--- code.py
+++ code.py
@@ -1,13 +1,22 @@
-n = int(input())
-for _ in range(n):
-    s = input().strip()
-    if not s or s[0] == '0':
-        print(0)
-        continue
-    max_len = len(s)
-    for i in range(1, len(s)):
-        if s[i-1] == '0' and s[i] == '1':
-            max_len = i
-            break
-    count = s[:max_len].count('0')
-    print(count)
+import sys
+
+MOD = 10**9 + 7
+max_fact = 10**5
+
+# Precompute factorials and inverse factorials up to max_fact
+fact = [1] * (max_fact + 1)
+for i in range(1, max_fact + 1):
+    fact[i] = fact[i-1] * i % MOD
+
+inv_fact = [1] * (max_fact + 1)
+inv_fact[max_fact] = pow(fact[max_fact], MOD-2, MOD)
+for i in range(max_fact - 1, -1, -1):
+    inv_fact[i] = inv_fact[i+1] * (i+1) % MOD
+
+def main():
+    input = sys.stdin.read().split()
+    ptr = 0
+    T = int(input[ptr])
+    ptr += 1
+    for _ in range(T):
+     
