9f4c82f848c3322333c92a8b6459e4383b0634d0
Analyzer <analyzer@test.com>
2026-02-09 13:42:40 -0800

    Update

--- code.py
+++ code.py
@@ -1,6 +1,13 @@
-n = int(input())
-a = list(map(int, input().split()))
-sum_a = sum(a)
-max_a = max(a)
-required = max(max_a, (sum_a + n - 2) // (n - 1))
-print(required)
+def generate_states(quests):
+    states = {(0, 0): (0, [])}
+    for li, mi, wi in quests:
+        new_states = {}
+        for (d_ab, d_bc), (sum_total, path) in states.items():
+            # Option LM
+            new_d_ab_lm = d_ab + (li - mi)
+            new_d_bc_lm = d_bc + mi
+            new_sum_lm = sum_total + li + mi
+            new_path_lm = path + ['LM']
+            key_lm = (new_d_ab_lm, new_d_bc_lm)
+            if key_lm not in new_states or new_sum_lm > new_states[key_lm][0]:
+        
