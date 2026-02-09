0f5c2ca84e490d5c7869bed3c9ebc8d6a6581199
Analyzer <analyzer@test.com>
2026-02-09 12:31:53 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,18 @@
-import sys
-import time
-import itertools
-from itertools import accumulate, product, permutations, combinations
-import collections
-from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
-from functools import lru_cache
-import math
-from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
-import fractions
-from typing import List, Tuple
-import numpy as np
-import random
-import heapq
-from heapq import *
-from dataclasses import dataclass
+def training(n,m):
+    t=0
+    for i in range(5*(10**5)):
+        if n>=m and n>1 and m>0:
+            n-=2
+            m-=1
+            t+=1
+        elif n<m and n>0 and m>1:
+            n-=1
+            m-=2
+            t+=1
+        else:
+            return t
 
-import builtins
-import re
+
+
+a,b=list(map(int,input().split(" ")))
+print(training(a,b))
