e7bc9270984f4e1a8ed5e34ef298ddaf1c49265f
Analyzer <analyzer@test.com>
2026-02-09 14:51:24 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,19 @@
-def lastSubstring(s: str) -> str:
-    n = len(s)
-    best = 0
-    for i in range(1, n):
-        current_length = 0
-        while True:
-            if best + current_length >= n or i + current_length >= n:
-                break
-            if s[best + current_length] != s[i + current_length]:
-                break
-            current_length += 1
-        if i + current_length >= n:
-            continue
-        if best + current_length >= n:
-            best = i
-            continue
-        if s[i 
+from collections import defaultdict
+
+def reorderedPowerOf2(N):
+    # Precompute the power_map once per function call (inefficient but acceptable)
+    power_map = defaultdict(set)
+    exp = 0
+    while True:
+        current_power = 2 ** exp
+        s = str(current_power)
+        length = len(s)
+        if length > 10:
+            break
+        sorted_str = ''.join(sorted(s))
+        power_map[length].add(sorted_str)
+        exp += 1
+    
+    s = str(N)
+    sorted_n = ''.join(sorted(s))
+    length
