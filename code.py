c3740d195d90fb42f40ecce81b867570cf45ee6a
Analyzer <analyzer@test.com>
2026-02-09 13:27:28 -0800

    Update

--- code.py
+++ code.py
@@ -1,4 +1,6 @@
-s = input().strip()
-parts = s.split('+')
-parts.sort()
-print('+'.join(parts))
+c = list(map(int, input().split()))
+total = sum(c)
+if total == 0 or total % 5 != 0:
+    print(-1)
+else:
+    print(total // 5)
