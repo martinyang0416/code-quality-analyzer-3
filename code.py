fbc25ad4c44605b1d323fdb2ea959066c6be71cd
Analyzer <analyzer@test.com>
2026-02-09 10:19:30 -0800

    Update

--- code.py
+++ code.py
@@ -1,13 +1,25 @@
-n = int(input())
-prefs = [list(map(int, input().split())) for _ in range(n)]
-gift = [0] * (n + 1)
-used = set()
+def main():
+    import sys
+    input = sys.stdin.read().split()
+    idx = 0
+    N = int(input[idx])
+    idx += 1
+    R = int(input[idx])
+    idx += 1
 
-for i in range(1, n + 1):
-    for j in prefs[i - 1]:
-        if j != i and j not in used:
-            gift[i] = j
-            used.add(j)
-            break
+    beds = []
+    for _ in range(N):
+        f = int(input[idx])
+        p = int(input[idx+1])
+        beds.append((p, f))
+        idx += 2
 
-print(' '.join(map(str, gift[1:])))
+    # Sort the flower beds by their positions
+    beds.sort()
+
+    # Extract positions and flowers
+    p = [x[0] for x in beds]
+    f = [x[1] for x in beds]
+
+    # Compute prefix sums
+    prefix = [0] * (N + 
