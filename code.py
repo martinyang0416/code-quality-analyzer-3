3bdaf6c6d69d61da4285d0d3bfa7cc47b368b347
Analyzer <analyzer@test.com>
2026-02-09 13:39:30 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,20 @@
-s = input().strip()
-a = c = m = 0
-for char in s:
-    if char == 'A':
-        a += 1
-    elif char == 'C':
-        c += a
-    elif char == 'M':
-        m += c
-print(m)
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    max_divisor = int(n ** 0.5) + 1
+    for i in range(3, max_divisor, 2):
+        if n % i == 0:
+            return False
+    return True
+
+n = int(input())
+count = 0
+for _ in range(n):
+    num = int(input())
+    if is_prime(num):
+        count += 1
+print(count)
