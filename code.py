ac8568fa3b7563cdd3b5bd2fa1aa2eb54b4ef333
Analyzer <analyzer@test.com>
2026-02-09 13:58:05 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,21 @@
-def largest_rectangle_area(heights):
-    stack = [-1]
-    max_area = 0
-    for i in range(len(heights)):
-        while stack[-1] != -1 and heights[i] < heights[stack[-1]]:
-            h = heights[stack.pop()]
-            w = i - stack[-1] - 1
-            max_area = max(max_area, h * w)
-        stack.append(i)
-    while stack[-1] != -1:
-        h = heights[stack.pop()]
-        w = len(heights) - stack[-1] - 1
-        max_area = max(max_area, h * w)
-    return max_area
+import sys
+import math
 
-h, w = map(int, input().spl
+def sieve(n):
+    sieve = [True] * (n + 1)
+    sieve[0] = sieve[1] = False
+    for i in range(2, int(math.isqrt(n)) + 1):
+        if sieve[i]:
+            sieve[i*i::i] = [False] * len(sieve[i*i::i])
+    primes = [i for i, is_p in enumerate(sieve) if is_p]
+    return primes
+
+base_primes = sieve(31623)
+
+t = int(sys.stdin.readline())
+for case in range(t):
+    m, n = map(int, sys.stdin.readline().split())
+    size = n - m + 1
+    is_prime = [True] * size
+    
+    for i in ra
