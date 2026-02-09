9632d157a6e8904c5c1f317627a19cd22e8fec2d
Analyzer <analyzer@test.com>
2026-02-09 14:22:14 -0800

    Update

--- code.py
+++ code.py
@@ -1,7 +1,22 @@
-def isValid(s):
-    stack = []
-    for c in s:
-        stack.append(c)
-        while len(stack) >= 3 and stack[-3:] == ['a', 'b', 'c']:
-            del stack[-3:]
-    return not stack
+def longestMountain(A):
+    max_len = 0
+    up = 0
+    down = 0
+    n = len(A)
+    if n < 3:
+        return 0
+    for i in range(1, n):
+        if A[i] > A[i-1]:
+            if down > 0:
+                up = 0
+                down = 0
+            up += 1
+        elif A[i] < A[i-1]:
+            if up > 0:
+                down += 1
+                current = up + down + 1
+                if current > max_len:
+                    max_len = current
+            else:
+                up = 0
+           
