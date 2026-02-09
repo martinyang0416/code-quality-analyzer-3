e07fc95d35a01d33ab7ea9fe2407797a4fac71df
Analyzer <analyzer@test.com>
2026-02-09 10:10:17 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,8 @@
-def main():
-    import sys
-    input = sys.stdin.read().split()
-    ptr = 0
-    while True:
-        N = int(input[ptr])
-        ptr += 1
-        if N == 0:
-            break
-        files = {}  # key: I, value: (start, end, E)
-        excl_intervals = []
-        free_intervals = [(0, float('inf'))]  # managed as list of (start, end), start <= end
-
-        for _ in range(N):
-            cmd = input[ptr]
-            ptr += 1
-            if cmd == 'W':
-                I = int(input[ptr])
-          
+n = int(input())
+total = 0.0
+for _ in range(n):
+    score = float(input())
+    total += score
+average = total / n
+adjusted_average = average + 10
+print("{0:.2f}".format(round(adjusted_average, 2)))
