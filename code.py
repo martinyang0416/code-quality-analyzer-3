b40f100bb61c297a25457a57c0eddfd156636240
Analyzer <analyzer@test.com>
2026-02-09 10:12:42 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,24 @@
-def count_odd_pairs(s):
-    even = 0
-    odd = 0
-    for char in s:
-        pos = ord(char) - ord('a') + 1
-        if pos % 2 == 0:
-            even += 1
-        else:
-            odd += 1
-    return even * odd
-
-T = int(input())
-for _ in range(T):
-    S = input().strip()
-    print(count_odd_pairs(S))
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    
+    n = int(data[0])
+    q = int(data[1])
+    a = list(map(int, data[2:n + 2]))
+    diff = [0] * (n + 2)
+    
+    index = n + 2
+    for i in range(q):
+        l = int(data[index])
+        r = int(data[index + 1])
+        index += 2
+        diff[l] += 1
+        if r + 1 <= n:
+            diff[r + 1] -= 1
+    
+    weights = []
+    current = 0
+    for i in range(1, n + 1):
+        current += diff[i]
+        weight
