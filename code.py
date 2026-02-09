922bf42df9a00cc460bfae0b795d9afd57537d2d
Analyzer <analyzer@test.com>
2026-02-09 13:55:45 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,12 @@
-import sys
+A, B, C, X, Y = map(int, input().split())
+max_k = 2 * max(X, Y)
+min_cost = float('inf')
 
-max_n = 10**6
+for k in range(0, max_k + 1):
+    a = max(0, (2 * X - k + 1) // 2)
+    b = max(0, (2 * Y - k + 1) // 2)
+    cost = a * A + b * B + k * C
+    if cost < min_cost:
+        min_cost = cost
 
-# Precompute smallest prime factors (SPF)
-spf = list(range(max_n + 1))
-for i in range(2, int(max_n**0.5) + 1):
-    if spf[i] == i:
-        for j in range(i*i, max_n + 1, i):
-            if spf[j] == j:
-                spf[j] = i
-
-# Precompute results for all possible n
-precomputed = [""] * (max_n + 1)
-for n in range(2, max_n + 1):
-    factors = {}
-    temp = n
-    while temp > 1:
-        p = spf[temp]
-        while temp % p == 0:
-            factors[p] = factors.get(p,
+print(min_cost)
