d9814e040c7d5bea81897b2f6072c7b709aa7915
Analyzer <analyzer@test.com>
2026-02-09 14:05:47 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,17 @@
 import sys
-from collections import deque
 
-def main():
-    sys.setrecursionlimit(1 << 25)
-    N, M = map(int, sys.stdin.readline().split())
-    values = [0] * (N + 1)  # 1-based indexing
-    for i in range(1, N+1):
-        values[i] = int(sys.stdin.readline())
-    adj = [[] for _ in range(N+1)]
-    in_degree = [0] * (N+1)
-    for _ in range(M):
-        u, v = map(int, sys.stdin.readline().split())
-        adj[u].append(v)
-        in_degree[v] += 1
-
-    # Compute topological order
-    queue = dequ
+def build_suffix_automaton(s):
+    states = [{'trans': {}, 'link': -1, 'len': 0}]
+    last = 0
+    for c in s:
+        curr = len(states)
+        states.append({'trans': {}, 'link': -1, 'len': states[last]['len'] + 1})
+        p = last
+        while p != -1 and c not in states[p]['trans']:
+            states[p]['trans'][c] = curr
+            p = states[p]['link']
+        if p == -1:
+            states[curr]['link'] = 0
+        else:
+            q = states[p]['trans'][c]
+            i
