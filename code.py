f86c7ce94209951b123f10ef6d0ce1a1da9f0e17
Analyzer <analyzer@test.com>
2026-02-09 11:22:44 -0800

    Update

--- code.py
+++ code.py
@@ -1,8 +1,18 @@
-import sys
+def main():
+    import sys
 
-while True:
-    try:
-        input().strip()  # Read the input line, ignore its content
-        print("NO", flush=True)
-    except EOFError:
-        break
+    prefix = "What are you doing while sending "  # 33 characters
+    middle_str = "? Are you busy? Will you send "  # 26 characters
+    f0_str = "What are you doing at the end of the world? Are you busy? Will you save us?"
+
+    q = int(sys.stdin.readline())
+    output = []
+
+    for _ in range(q):
+        n, k = map(int, sys.stdin.readline().split())
+        current_level = n
+        current_k = k
+
+        while current_level > 0:
+            if current_k <= 33:
+     
