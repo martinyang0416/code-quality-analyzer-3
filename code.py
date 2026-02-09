99999d4069b276623c65cd37ea6683417ea9bdc4
Analyzer <analyzer@test.com>
2026-02-09 12:58:47 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,15 @@
-import sys
-import time
-import itertools
-from itertools import accumulate, product, permutations, combinations
-import collections
-from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
-from functools import lru_cache
-import math
-from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
-import fractions
-from typing import List, Tuple
-import numpy as np
-import random
-import heapq
-from heapq import *
-from dataclasses import dataclass
+def t(b, pr):
+    i, p = 1, [0, 0]
+    while b[0] or b[1]:
+        if i == 0:
+            c = pr if b[pr] else 1 - pr
+        else:
+            c = 1 - pr if b[1 - pr] else pr
+        p[c != pr] += 1
+        b[c] -= 1
+        i, pr = 1 - i, c
+    return p
 
-import builtins
-import re
+n, m = map(int, input().split())
+v1, v2 = t([n - 1, m], 0), t([n, m - 1], 1)
+print(' '.join(map(str, v1 if v1[0] >= v2[0] else v2)))
