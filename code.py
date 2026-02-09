41067ada98278ff6cc2a6c1643452dcf842509a4
Analyzer <analyzer@test.com>
2026-02-09 14:12:01 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,25 @@
 import sys
 
-def main():
-    n, q = map(int, sys.stdin.readline().split())
-    a = list(map(int, sys.stdin.readline().split()))
-    max_a = max(a) if a else 0
+class State:
+    __slots__ = ['len', 'link', 'next']
+    def __init__(self):
+        self.len = 0
+        self.link = -1
+        self.next = dict()
 
-    def compute_mobius(max_mobius):
-        if max_mobius < 1:
-            return []
-        spf = list(range(max_mobius + 1))
-        for i in range(2, int(max_mobius**0.5) + 1):
-            if spf[i] == i:
-                for j in range(i * i, max_mobius + 1, i):
-                    if spf[j] == j:
-                        spf[j] = i
-    
+def main():
+    input = sys.stdin.read().split()
+    T = int(input[0])
+    cases = input[1:T+1]
+    for s in cases:
+        sa = [State()]
+        sa[0].link = -1
+        sa[0].len = 0
+        last = 0
+        size = 1
+        cnt = [0]
+        for c in s:
+            p = last
+            curr = size
+            sa.append(State())
+      
