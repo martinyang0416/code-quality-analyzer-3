bb59027471004542ae421ff226079455048287e8
Analyzer <analyzer@test.com>
2026-02-09 12:37:42 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,19 @@
-def f(s):
-    n  = len(s)
-    c  = 0
-    ll = 0
-    for i in range(n-3):
-        if s[i:i+4] == 'bear':
-            l  = i-ll+1
-            r  = n-i-3
-            c += l*r
-            ll = i+1
-    return c
+import sys
+import time
+import itertools
+from itertools import accumulate, product, permutations, combinations
+import collections
+from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
+from functools import lru_cache
+import math
+from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
+import fractions
+from typing import List, Tuple
+import numpy as np
+import random
+import heapq
+from heapq import *
+from dataclasses import dataclass
 
-s = input()
-print(f(s))
+import builtins
+import re
