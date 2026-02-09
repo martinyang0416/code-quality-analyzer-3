858e38a96efbb860c595b64df8e81673601915eb
Analyzer <analyzer@test.com>
2026-02-09 10:07:44 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,11 @@
-def main():
-    import sys
-    input = sys.stdin.read().split()
-    idx = 0
-    t = int(input[idx])
-    idx += 1
-    for _ in range(t):
-        n = int(input[idx])
-        k = int(input[idx + 1])
-        idx += 2
-        arr = list(map(int, input[idx:idx + n]))
-        idx += n
+b = int(input())
+m = input().strip()
 
-        # Find the top k elements
-        top_k = sorted(arr, reverse=True)[:k]
-        top_set = set(top_k)
-        count = 0
-        for num in arr[:k]:
-            if num in top_set:
-                count += 1
-       
+if m == '0':
+    print(0)
+else:
+    res = 0
+    for c in m:
+        d = int(c)
+        res = res * b + d
+    print(res)
