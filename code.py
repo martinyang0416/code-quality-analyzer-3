608d527dd60bd13b8b761cb6b1257dbdaee6edde
Analyzer <analyzer@test.com>
2026-02-09 10:36:03 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,11 @@
-import sys
-
-def main():
-    input = sys.stdin.read().split()
-    idx = 0
-    T = int(input[idx])
-    idx += 1
-    for _ in range(T):
-        K = int(input[idx])
-        N = int(input[idx + 1])
-        idx += 2
-        
-        # Compute mod8 part
-        T8 = 8
-        m8, r8 = divmod(K, T8)
-        sum_p8 = sum(pow(i, N, 8) for i in range(1, T8 + 1))
-        sum_r8 = sum(pow(i, N, 8) for i in range(1, r8 + 1))
-        mod8 = (m8 * sum_p8 + sum_r8) % 8
-        
-        # Compute mod125 part
-    
+k = int(input())
+for i in range(1, k):
+    row = []
+    for j in range(1, k):
+        if j == 0:
+            row.append("N/A")
+        elif i % j == 0:
+            row.append(str(i // j))
+        else:
+            row.append("N/A")
+    print(' '.join(row))
