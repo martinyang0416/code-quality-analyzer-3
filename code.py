a227d8d5374b606a0a8aea4a1a690798f93e2526
Analyzer <analyzer@test.com>
2026-02-09 11:19:10 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,3 @@
+allowed = {'3', '4', '6', '7'}
 s = input().strip()
-sum_digits = sum(int(c) for c in s)
-
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-print("Yes" if is_prime(sum_digits) else "No")
+print("Yes" if all(c in allowed for c in s) else "No")
