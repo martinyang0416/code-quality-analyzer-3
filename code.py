41a5ab4ca9f2adf6ecd4907f48c9a1bfbe9b9637
Analyzer <analyzer@test.com>
2026-02-09 15:18:36 -0800

    Update

--- code.py
+++ code.py
@@ -1,4 +1,14 @@
-T = int(input())
-for _ in range(T):
-    nums = sorted(map(int, input().split()))
-    print(nums[1])
+import sys
+
+def main():
+    valid = {1, 2, 145, 40585}
+    input = sys.stdin.read().split()
+    T = int(input[0])
+    results = []
+    for n_str in input[1:T+1]:
+        n = int(n_str)
+        results.append(1 if n in valid else 0)
+    print('\n'.join(map(str, results)))
+
+if __name__ == "__main__":
+    main()
