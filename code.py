bcfea108791cf32ca024d240a698b135da52bd1a
Analyzer <analyzer@test.com>
2026-02-09 14:29:34 -0800

    Update

--- code.py
+++ code.py
@@ -1,13 +1,10 @@
-def jump(nums):
-    jumps = 0
-    current_end = 0
-    max_far = 0
-    n = len(nums)
-    for i in range(n - 1):
-        max_far = max(max_far, i + nums[i])
-        if i == current_end:
-            jumps += 1
-            current_end = max_far
-            if current_end >= n - 1:
-                break
-    return jumps
+def wiggleMaxLength(nums):
+    if not nums:
+        return 0
+    up = down = 1
+    for i in range(1, len(nums)):
+        if nums[i] > nums[i-1]:
+            up = down + 1
+        elif nums[i] < nums[i-1]:
+            down = up + 1
+    return max(up, down)
