454209d83a73df4c6018c4368266bef6c3570666
Analyzer <analyzer@test.com>
2026-02-09 11:21:23 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,5 @@
-s = input().strip()
-digits = list(map(int, s[1:]))  # Extract the six digits after 'A'
+import sys
 
-sum_digits = sum(digits)
-has_zero = 0 in digits
-
-if has_zero:
-    print(sum_digits + 10)
-else:
-    print(sum_digits + 1)
+for line in sys.stdin:
+    print("NO")
+    sys.stdout.flush()
