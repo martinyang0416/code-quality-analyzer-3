5fa48a603f68bc73321cf5a479c9cd130246b898
Analyzer <analyzer@test.com>
2026-02-09 13:02:34 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,7 @@
-import bisect
-
-def count_balanced_substrings(s):
-    n = len(s)
-    count_4 = [0] * (n + 1)
-    for i in range(1, n+1):
-        count_4[i] = count_4[i-1] + (1 if s[i-1] == '4' else 0)
-    ans = 0
-    for L in range(1, n+1):
-        a = count_4[L-1]
-        # Binary search for the smallest R >= L where count_4[R] > a
-        # The count_4 array is non-decreasing, so we can use bisect
-        # We need to search in the subarray count_4[L..n]
-        # Using bisect_right to find the first element >
+q = int(input())
+for _ in range(q):
+    l, r, d = map(int, input().split())
+    if d < l:
+        print(d)
+    else:
+        print(((r // d) + 1) * d)
