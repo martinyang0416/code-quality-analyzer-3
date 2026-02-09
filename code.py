475c518e13eb92c7eba0de0bae864945f4c26508
Analyzer <analyzer@test.com>
2026-02-09 14:27:41 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,20 @@
-def largestValsFromLabels(values, labels, num_wanted, use_limit):
-    items = sorted(zip(values, labels), key=lambda x: -x[0])
-    label_counts = {}
-    total = 0
-    taken = 0
-    for value, label in items:
-        if taken >= num_wanted:
-            break
-        current = label_counts.get(label, 0)
-        if current < use_limit:
-            total += value
-            label_counts[label] = current + 1
-            taken += 1
-    return total
+def numSteps(s):
+    def add_one(s):
+        s_list = list(s)
+        carry = 1
+        i = len(s_list) - 1
+        while i >= 0 and carry:
+            total = int(s_list[i]) + carry
+            if total == 2:
+                s_list[i] = '0'
+                carry = 1
+            else:
+                s_list[i] = '1'
+                carry = 0
+            i -= 1
+        if carry:
+            s_list.insert(0, '1')
+        return ''.join(s_list)
+    
+    def count_trailing_zeros(s):
+        count = 
