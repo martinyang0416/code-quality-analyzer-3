b2664d112078a33cb25de3b79ce0857f3a34235f
Analyzer <analyzer@test.com>
2026-02-09 10:24:17 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,7 @@
-import math
-
-def minimal_additional_matches():
-    n = int(input())
-    for _ in range(n):
-        w, t, a, b = map(int, input().split())
-        if a == 0:
-            if w == 0:
-                print(0)
-            else:
-                print(-1)
-            continue
-        
-        g = math.gcd(a, b)
-        a_prime = a // g
-        b_prime = b // g
-        
-        if a_prime > b_prime:
-            print(-1)
-            continue
-        
-        if a_prime == b_prime:
-            if w == t:
+n = int(input())
+result = {
+    'candies': 4 * n,
+    'chocolates': 2 * n,
+    'toys': n
+}
+print(result)
