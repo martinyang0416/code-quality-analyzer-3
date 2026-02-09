6bf453c4e009d80f54e0b23a9f88ff98feaa0cb7
Analyzer <analyzer@test.com>
2026-02-09 11:28:58 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,19 @@
-from typing import List
 from collections import deque
 
 class Edge:
-    def __init__(self, to: int, rev: int, capacity: int):
+    def __init__(self, to, rev, capacity):
         self.to = to
         self.rev = rev
         self.capacity = capacity
 
 class Dinic:
-    def __init__(self, n: int):
+    def __init__(self, n):
         self.size = n
         self.graph = [[] for _ in range(n)]
-        
-    def add_edge(self, fr: int, to: int, capacity: int):
-        forward = Edge(to, len(self.graph[to]), capacity)
+    
+    def add_edge(self, fr, to, cap):
+        forward = Edge(to, len(self.graph[to]), cap)
         backward = Edge(fr, len(self.graph[fr]), 0)
-        se
+        self.graph[fr].append(forward)
+        self.graph[to].append(backward)
+    
