8173bebfdb0886908ba0d5fb18d35f8473b1297d
Analyzer <analyzer@test.com>
2026-02-09 14:00:00 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,17 @@
-import sys
-from collections import deque
-
-class Edge:
-    def __init__(self, to, rev, cap):
-        self.to = to
-        self.rev = rev
-        self.cap = cap
-
-class Dinic:
-    def __init__(self, n):
-        self.size = n
-        self.graph = [[] for _ in range(n)]
-    
-    def add_edge(self, fr, to, cap):
-        forward = Edge(to, len(self.graph[to]), cap)
-        backward = Edge(fr, len(self.graph[fr]), 0)
-        self.graph[fr].append(forward)
-        self.graph[to].append(backward)
-    
-   
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    a = [list(map(int, input().split())) for _ in range(n)]
+    for i in range(n):
+        row = []
+        for j in range(m):
+            if (i + j) % 2 == 0:
+                if a[i][j] % 2 == 0:
+                    row.append(a[i][j])
+                else:
+                    row.append(a[i][j] + 1)
+            else:
+                if a[i][j] % 2 == 1:
+                    row.append(a[i][j])
+                else:
+       
