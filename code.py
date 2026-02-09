588a900903554d1ac8f9c0395904a026d9f29a0e
Analyzer <analyzer@test.com>
2026-02-09 13:31:50 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,21 @@
-import math
-
-def get_divisors(n):
-    divisors = set()
-    for i in range(1, int(math.isqrt(n)) + 1):
-        if n % i == 0:
-            divisors.add(i)
-            divisors.add(n // i)
-    return divisors
+import sys
 
 def main():
-    import sys
     input = sys.stdin.read().split()
-    N = int(input[0])
-    a = list(map(int, input[1:N+1]))
-    a_sorted = sorted(a)
-    
-    divisors = set()
-    for num in a_sorted:
-        divisors.update(get_divisors(num))
-    
-    # Add values around the first element
-    
+    ptr = 0
+    T = int(input[ptr])
+    ptr += 1
+    for _ in range(T):
+        N, M, K = map(int, input[ptr:ptr+3])
+        ptr += 3
+        A = list(map(int, input[ptr:ptr+N]))
+        ptr += N
+        B = []
+        for _ in range(N):
+            row = list(map(int, input[ptr:ptr+M]))
+            B.append(row)
+            ptr += M
+        C = []
+        for _ in range(N):
+            row = list(map(int, input[ptr:ptr+M]))
+          
