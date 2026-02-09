d6ad575e427c3b74d0b48b5e812af8b339a20ea5
Analyzer <analyzer@test.com>
2026-02-09 13:51:07 -0800

    Update

--- code.py
+++ code.py
@@ -1,28 +1,20 @@
 n = int(input())
-a = list(map(int, input().split()))
+left0 = 0
+left1 = 0
+right0 = 0
+right1 = 0
 
-S = [i+1 for i, val in enumerate(a) if val == 1]
-T = [i+1 for i, val in enumerate(a) if val == 0]
+for _ in range(n):
+    l, r = map(int, input().split())
+    if l == 0:
+        left0 += 1
+    else:
+        left1 += 1
+    if r == 0:
+        right0 += 1
+    else:
+        right1 += 1
 
-S.sort()
-T.sort()
-
-m = len(S)
-k = len(T)
-
-if m == 0:
-    print(0)
-    exit()
-
-INF = 10**18
-
-prev_dp = [0] * (k + 1)
-
-for i in range(1, m + 1):
-    curr_dp = [INF] * (k + 1)
-    for j in range(1, k + 1):
-        if j < i:
-            curr_dp[j] = INF
-            continue
-        option1 = prev_dp[j-1] + abs(S[i-1] - T[j-1])
-        option2 = curr
+left_min = min(left0, left1)
+right_min = min(right0, right1)
+print(left_min + right_min)
