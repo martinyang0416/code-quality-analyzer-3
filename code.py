48e29937d4a1fad86cf6798884008e67a8e3fbcd
Analyzer <analyzer@test.com>
2026-02-09 12:00:34 -0800

    Update

--- code.py
+++ code.py
@@ -1,3 +1,20 @@
-A bracketed sequence is called correct (regular) if by inserting "+" and "1" you can get a well-formed mathematical expression from it. For example, sequences "(())()", "()" and "(()(()))" are correct, while ")(", "(()" and "(()))(" are not.
-
-The teacher gave Dmitry's class a very strange task — she asked every student to come up with a sequence of arbitrary length, consisting only of opening and closing brackets. After that all the students took turns naming the sequences they had invented. Whe
+from typing import List
+def pyramid():
+    n = int(input())
+    aux = n
+    respuesta = 1
+    altura = 1
+    if(n<2):
+        respuesta = 0
+    while(aux>=2):
+        aux = aux - 2*altura - altura + 1
+        if( aux < 0 ):
+            aux = aux + 2*altura + altura - 1
+            altura = 1
+            respuesta = respuesta + 1
+        else:
+            altura = altura + 1
+    print(respuesta)
+t = int(input())
+for i in range(t):
+    pyramid()
