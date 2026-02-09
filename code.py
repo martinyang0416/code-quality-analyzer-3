d7497e2351825b8ca277c12c51c3e9573da25b93
Analyzer <analyzer@test.com>
2026-02-09 10:27:14 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,12 @@
-m, p, q, t = map(int, input().split())
+n, m = map(int, input().split())
+scores = list(map(int, input().split()))
 
-total = t ** q
+excluded = []
+for i in range(n):
+    if scores[i] < m:
+        excluded.append(i + 1)  # 1-based index
 
-if m > total:
-    print(-1)
-else:
-    packages = []
-    for i in range(m):
-        number = i
-        digits = []
-        for _ in range(q):
-            digits.append(number % t)
-            number = number // t
-        digits = digits[::-1]  # Reverse to get the correct order
-        truck_assignment = [d + 1 for d in digits]
-        packages.append(truck_assignment)
-    
-    for day in range(q):
-        line = ' '.join(map(str, [pkg[day] 
+excluded.sort()
+
+for idx in excluded:
+    print(idx)
