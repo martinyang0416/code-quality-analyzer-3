fed255fa9c087b13ebe97ac79439eb31e8c220cc
Analyzer <analyzer@test.com>
2026-02-09 13:25:26 -0800

    Update

--- code.py
+++ code.py
@@ -1,30 +1,10 @@
-import sys
-
-def query(c, d):
-    print(f"? {c} {d}")
-    sys.stdout.flush()
-    return int(sys.stdin.readline())
-
-a = 0
-b = 0
-
-res = query(0, 0)
-if res == 0:
-    print(f"! {a} {b}")
-    sys.exit()
-
-# Find the highest differing bit
-m = 29
-for bit in range(29, -1, -1):
-    new_res = query(a ^ (1 << bit), b)
-    if new_res != res:
-        m = bit
-        break
-
-# Determine the m-th bit (a has 1, b has 0)
-a ^= (1 << m)
-
-# Determine remaining bits
-for bit in range(m-1, -1, -1):
-    # Check a's bit
-  
+t = int(input())
+for _ in range(t):
+    a, b, c = map(int, input().split())
+    max_ops = 0
+    x_max = min(a, b // 2)
+    for x in range(x_max + 1):
+        current_b = b - 2 * x
+        y = min(current_b, c // 2)
+        max_ops = max(max_ops, x + y)
+    print(max_ops * 3)
