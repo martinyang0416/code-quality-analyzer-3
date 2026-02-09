5a925c6abcc7a619fdfd44bc18c226f7f30c1963
Analyzer <analyzer@test.com>
2026-02-09 14:13:26 -0800

    Update

--- code.py
+++ code.py
@@ -1,7 +1,15 @@
-def superPow(a, b):
-    mod = 1337
-    a_mod = a % mod
-    result = 1
-    for digit in b:
-        result = (pow(result, 10, mod) * pow(a_mod, digit, mod)) % mod
-    return result
+def mincostTickets(days, costs):
+    travel_days = set(days)
+    max_day = days[-1]
+    dp = [0] * (max_day + 1)
+    
+    for day in range(1, max_day + 1):
+        if day not in travel_days:
+            dp[day] = dp[day - 1]
+        else:
+            cost1 = dp[day - 1] + costs[0]
+            cost7 = costs[1] if day - 7 < 0 else dp[day - 7] + costs[1]
+            cost30 = costs[2] if day - 30 < 0 else dp[day - 30] + costs[2]
+            dp[day] = min(cost1, cost7, cost30)
+    
+    return dp[max_
