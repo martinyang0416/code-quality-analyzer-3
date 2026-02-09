48ca333835f0cb5640efc11b2c3d1e8ea4ce4751
Analyzer <analyzer@test.com>
2026-02-09 11:32:53 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,21 @@
-import sys
-from collections import deque
+import bisect
 
 def main():
-    sys.setrecursionlimit(1 << 25)
-    N = int(sys.stdin.readline())
-    s = sys.stdin.readline().strip()
-    edges = [[] for _ in range(N+1)]
-    for _ in range(N-1):
-        a, b = map(int, sys.stdin.readline().split())
-        edges[a].append(b)
-        edges[b].append(a)
-    
-    # Find all cowlix nodes
-    cowlix_nodes = []
+    import sys
+    input = sys.stdin.read().split()
+    N = int(input[0])
+    a = list(map(int, input[1:N+1]))
+
+    prefix = [0] * (N + 1)
     for i in range(1, N+1):
-        if s[i-1] == '1':
-            cowlix_nodes.append(i)
-    M = len(cowlix_nodes)
+        prefix[i] = prefix[i-1] + a[i-1]
+
+    for i in range(1, N+1):
+        # Compute T: subarrays not including i
+        left_T = []
+        if i > 1:
+            left_T = [prefix[e] - prefix[s-1] for s in range(1, i) for e in range(s, i)]
+        
+        right_T = []
+        if i < N:
+            ri
