acdf19bdc48e54fa4f950326c91b598089d37ef3
Analyzer <analyzer@test.com>
2026-02-09 10:13:15 -0800

    Update

--- code.py
+++ code.py
@@ -1,24 +1,26 @@
+import sys
+sys.setrecursionlimit(1 << 25)
+
 def main():
     import sys
     input = sys.stdin.read
     data = input().split()
-    
-    n = int(data[0])
-    q = int(data[1])
-    a = list(map(int, data[2:n + 2]))
-    diff = [0] * (n + 2)
-    
-    index = n + 2
-    for i in range(q):
-        l = int(data[index])
-        r = int(data[index + 1])
-        index += 2
-        diff[l] += 1
-        if r + 1 <= n:
-            diff[r + 1] -= 1
-    
-    weights = []
-    current = 0
-    for i in range(1, n + 1):
-        current += diff[i]
-        weight
+    idx = 0
+    n = int(data[idx])
+    idx += 1
+    m = int(data[idx])
+    idx += 1
+
+    parent = [i for i in range(n + 1)]  # 1-based indexing
+    rank = [1] * (n + 1)
+
+    def find(u):
+        while parent[u] != u:
+            parent[u] = parent[parent[u]]
+            u = parent[u]
+        return u
+
+    def union(u, v):
+        u_root = find(u)
+        v_root = find(v)
+  
