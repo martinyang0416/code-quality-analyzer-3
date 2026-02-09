02d47aed4355dae648a10dbf77e62059f5dc4669
Analyzer <analyzer@test.com>
2026-02-09 13:28:00 -0800

    Update

--- code.py
+++ code.py
@@ -1,6 +1,16 @@
-c = list(map(int, input().split()))
-total = sum(c)
-if total == 0 or total % 5 != 0:
-    print(-1)
-else:
-    print(total // 5)
+import sys
+
+users = set()
+total = 0
+
+for line in sys.stdin:
+    line = line.rstrip('\n')
+    if line.startswith('+'):
+        users.add(line[1:])
+    elif line.startswith('-'):
+        users.remove(line[1:])
+    else:
+        sender, message = line.split(':', 1)
+        total += len(message) * len(users)
+
+print(total)
