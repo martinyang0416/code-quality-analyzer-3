a4b14f9c7afc9469ef4b452472582d4afc9570fe
Analyzer <analyzer@test.com>
2026-02-09 12:02:11 -0800

    Update

--- code.py
+++ code.py
@@ -1,25 +1,19 @@
-EPS = 1e-4
+import sys
+import time
+import itertools
+from itertools import accumulate, product, permutations, combinations
+import collections
+from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
+from functools import lru_cache
+import math
+from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
+import fractions
+from typing import List, Tuple
+import numpy as np
+import random
+import heapq
+from heapq import *
+from dataclasses import dataclass
 
-#外積
-def OuterProduct(one, two):
-	tmp = one.conjugate() * two
-	return tmp.imag
-
-#内積
-def InnerProduct(one, two):
-	tmp = one.conjugate() * two
-	return tmp.real
-
-def solve(a, b, c, d):
-	if abs(OuterProduct(b-a, d-c)) <= EPS:
-		return 2
-	elif abs(InnerProduct(b-a, d-c)) <= EPS:
-		return 1
-	else:
-		return 0
-
-n = int(input())
-for _ in range(n):
-	pp = list(map(int, input().split()))
-	p = [complex(pp[i], pp[i+1]) for i in range(0, 8, 2)]
-	print(solve(p[0], p[1], p[2], p[3]))
+import builtins
+import re
