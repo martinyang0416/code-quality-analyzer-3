ee2d71b1a135d26b6c014ad59fc37f16faaf57fa
Analyzer <analyzer@test.com>
2026-02-09 13:40:40 -0800

    Update

--- code.py
+++ code.py
@@ -1,24 +1,17 @@
-def decimal_to_septenary(n):
-    if n == 0:
-        return '0'
-    is_negative = False
-    if n < 0:
-        is_negative = True
-        n = -n
-    digits = []
-    while n > 0:
-        digits.append(str(n % 7))
-        n = n // 7
-    res = ''.join(reversed(digits))
-    if is_negative:
-        res = '-' + res
-    return res
+MOD = 998244353
+MAX = 10**6  # Adjust based on constraints to avoid MLE, but 1e6 is manageable.
 
-nums = []
-for num in map(int, input().split()):
-    if num == -1:
-        break
-    nums.append(num)
+# Precompute factorial, inverse factorial, and inverses up to MAX
+fact = [1] * (MAX)
+for i in range(1, MAX):
+    fact[i] = fact[i-1] * i % MOD
 
-septenary = [decimal_to_septenary(num) for num in nums]
-print(' '.join(se
+inv_fact = [1] * (MAX)
+inv_fact[MAX-1] = pow(fact[MAX-1], MOD-2, MOD)
+for i in range(MAX-2, -1, -1):
+    inv_fact[i] = inv_fact[i+1] * (i+1) % MOD
+
+def C(n, k):
+    if n < 0 or k < 0 or n < k:
+        return 0
+    return fact[n] * inv_fact[k] % MOD * inv_fact
