9901dadf489f1f0da88d33f184b9061cfc69c0ba
Analyzer <analyzer@test.com>
2026-02-09 14:01:37 -0800

    Update

--- code.py
+++ code.py
@@ -1,25 +1,19 @@
-import sys
+import bisect
 
-a, b = map(int, sys.stdin.readline().split())
+class SegmentTree:
+    def __init__(self, size):
+        self.n = 1
+        while self.n < size:
+            self.n <<= 1
+        self.size = size
+        self.tree = [-float('inf')] * (2 * self.n)
 
-if a == 0:
-    print(-b * b)
-    print('x' * b)
-    sys.exit(0)
-if b == 0:
-    print(a * a)
-    print('o' * a)
-    sys.exit(0)
-
-max_k = min(a, b - 1)
-best_score = -float('inf')
-best_k = 1
-
-for k in range(1, max_k + 1):
-    m = k + 1
-    qo, ro = divmod(a, k)
-    sum_os = ro * (qo + 1) ** 2 + (k - ro) * qo ** 2
-    qx, rx = divmod(b, m)
-    sum_xs = rx * (qx + 1) ** 2 + (m - rx) * qx ** 2
-    current = sum_os - sum_xs
-    if current > bes
+    def update_point(self, pos, value):
+        pos += self.n
+        self.tree[pos] = value
+        pos >>= 1
+        while pos >= 1:
+            new_val = max(self.tree[2 * pos], self.tree[2 * pos + 1])
+            if self.tree[pos] == new_val:
+                break
+            self.
