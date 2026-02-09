9f541d0b7fcc6fd673ef67d1e1071dc4d2e3ab47
Analyzer <analyzer@test.com>
2026-02-09 14:47:45 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,19 @@
-def smallest_repunit_divisible_by_k(K):
-    if K % 2 == 0 or K % 5 == 0:
-        return -1
-    mod_val = 9 * K
-    current = 1
-    for m in range(1, mod_val + 1):
-        current = (current * 10) % mod_val
-        if current == 1:
-            return m
-    return -1
+def maxSumDivThree(nums):
+    total = sum(nums)
+    remainder = total % 3
+    if remainder == 0:
+        return total
+    
+    mod1 = sorted(num for num in nums if num % 3 == 1)
+    mod2 = sorted(num for num in nums if num % 3 == 2)
+    
+    possible = []
+    if remainder == 1:
+        if mod1:
+            possible.append(mod1[0])
+        if len(mod2) >= 2:
+            possible.append(mod2[0] + mod2[1])
+    else:  # remainder == 2
+        if mod2:
+            possible.append(mod2[0])
+        if 
