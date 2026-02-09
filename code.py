a37e05e7ee06f3689e002a7bea6bfcce3f4525f7
Analyzer <analyzer@test.com>
2026-02-09 10:26:26 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,18 @@
-h, w = map(int, input().split())
-grid = [input().strip() for _ in range(h)]
+n = int(input())
+intervals = []
+for _ in range(n):
+    s, e = map(int, input().split())
+    intervals.append((s, e))
 
-# Initialize horizontal and vertical matrices
-horizontal = [[0 for _ in range(w)] for __ in range(h)]
-vertical = [[0 for _ in range(w)] for __ in range(h)]
-
-for i in range(h):
-    for j in range(w):
-        # Check for horizontal pairs
-        if j < w - 1 and grid[i][j] == '.' and grid[i][j+1] == '.':
-            horizontal[i][j] = 1
+def calculate_coverage(intervals_list):
+    if not intervals_list:
+        return 0
+    sorted_intervals = sorted(intervals_list, key=lambda x: x[0])
+    merged = []
+    for interval in sorted_intervals:
+        if not merged:
+            merged.append(list(interval))
         else:
-            horizontal[i][j] = 0
-        # Check for vertical pairs
-    
+            last_start, last_end = merged[-1]
+            current_start, current_end = interval
+   
