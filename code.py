8bc8d71cca9773ea6d0de063397e287c4c72f737
Analyzer <analyzer@test.com>
2026-02-09 13:33:54 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,27 @@
-import sys
+n = int(input())
+a = list(map(int, input().split()))
 
-def main():
-    input = sys.stdin.read
-    data = input().split()
-    idx = 0
-    t = int(data[idx])
-    idx += 1
-    for _ in range(t):
-        n = int(data[idx])
-        idx += 1
-        a = list(map(int, data[idx:idx+n]))
-        idx += n
-        if n == 1:
-            print("Yes")
-            continue
-        
-        # Compute left_valid and is_left_possible
-        left_valid = [False] * n
-        for i in range(n):
-            left_valid[i] = a[i] >= i
-        
-        is_left
+if n == 1:
+    print("1 1")
+    print(-a[0])
+    print("1 1")
+    print(0)
+    print("1 1")
+    print(0)
+else:
+    # Operation 1: zero the first element
+    print("1 1")
+    print(-a[0])
+    a[0] = 0
+
+    # Operation 2: handle elements 2 to n
+    print(f"2 {n}")
+    ks = []
+    add_op2 = []
+    for i in range(1, n):
+        k = a[i] % n
+        ks.append(k)
+        add_op2.append(k * (n-1))
+    print(' '.join(map(str, add_op2)))
+
+    # Operat
