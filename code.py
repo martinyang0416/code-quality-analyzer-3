2f6026aa5dfdb75d4ef8ad2710f3f131d0cb5c88
Analyzer <analyzer@test.com>
2026-02-09 14:04:34 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,19 @@
-n, s = map(int, input().split())
-A = list(map(int, input().split()))
-mod = 998244353
+import sys
+from collections import deque
 
-prev_count = [0] * (s + 1)
-prev_sum_x1 = [0] * (s + 1)
-
-answer = 0
-
-for i in range(n):
-    a = A[i]
-    curr_count = [0] * (s + 1)
-    curr_sum_x1 = [0] * (s + 1)
-    
-    if a <= s:
-        curr_count[a] += 1
-        curr_sum_x1[a] = (curr_sum_x1[a] + (i + 1)) % mod
-    
-    for s_prev in range(s + 1):
-        if s_prev + a > s:
-            continue
-        s_new = s_prev + a
-        curr_count[s_new] = (curr_
+def main():
+    while True:
+        line = sys.stdin.readline().strip()
+        while line == '':
+            line = sys.stdin.readline().strip()
+        n, m = map(int, line.split())
+        if n == 0 and m == 0:
+            break
+        grid = []
+        target = None
+        for i in range(m):
+            row = sys.stdin.readline().strip()
+            grid.append(row)
+            for j in range(n):
+                if row[j] == '&':
+                  
