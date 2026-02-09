0f253900577e512f598f206106ed6de7edb5e1c1
Analyzer <analyzer@test.com>
2026-02-09 13:49:58 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,19 @@
 import sys
 
 def main():
-    input = sys.stdin.read
-    data = input().split()
-    ptr = 0
-    T = int(data[ptr])
-    ptr += 1
-    for _ in range(T):
-        n = int(data[ptr])
-        m = int(data[ptr+1])
-        ptr +=2
-        edges = []
-        for i in range(m):
-            u = int(data[ptr])
-            v = int(data[ptr+1])
-            edges.append((u, v))
-            ptr +=2
-        used = [False] * (3 * n + 2)
-        matching = []
-        for i in range(m):
-            u, v = edges[i]
-  
+    t = int(sys.stdin.readline())
+    for _ in range(t):
+        n = int(sys.stdin.readline())
+        a = list(map(int, sys.stdin.readline().split()))
+        s = [(i + x) % n for i, x in enumerate(a)]
+        freq = [0] * n
+        valid = True
+        for num in s:
+            if freq[num] != 0:
+                valid = False
+                break
+            freq[num] += 1
+        print("YES" if valid else "NO")
+
+if __name__ == "__main__":
+    main()
