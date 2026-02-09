49ade0c5b68c147ad93077efb82d12e4c9383c27
Analyzer <analyzer@test.com>
2026-02-09 13:29:00 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,21 @@
-import bisect
+import sys
+from sys import stdin
+from collections import deque
 
-n, k, s, t = map(int, input().split())
-cars = [tuple(map(int, input().split())) for _ in range(n)]
-g = list(map(int, input().split()))
-
-# Process checkpoints and compute segments
-checkpoints = [0] + g + [s]
-checkpoints.sort()
-segments = []
-for i in range(1, len(checkpoints)):
-    segments.append(checkpoints[i] - checkpoints[i-1])
-
-segments.sort()
-
-# Compute prefix sums and max_gap
-prefix = [0]
-current_sum = 0
-for d in segments:
-    current_sum += d
-    prefix.append(current_sum)
-s
+def main():
+    n, m = map(int, stdin.readline().split())
+    forbidden = [set() for _ in range(n+1)]
+    for _ in range(m):
+        x, y = map(int, stdin.readline().split())
+        forbidden[x].add(y)
+        forbidden[y].add(x)
+    
+    remaining = set(range(1, n+1))
+    components = []
+    
+    while remaining:
+        u = remaining.pop()
+        component = 1
+        queue = deque([u])
+        while queue:
+            current =
