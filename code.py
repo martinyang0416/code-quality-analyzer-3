322f71016f235120e713b41a55fc5016eb5c82b5
Analyzer <analyzer@test.com>
2026-02-09 14:20:42 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,7 @@
-from typing import List
-
-class Solution:
-    def minOperationsMaxProfit(self, customers: List[int], boardingCost: int, runningCost: int) -> int:
-        waiting = 0
-        boarded = 0
-        rotations = 0
-        max_profit = -float('inf')
-        best_rotations = -1
-        i = 0
-        n = len(customers)
-        
-        while i < n or waiting > 0:
-            if i < n:
-                waiting += customers[i]
-                i += 1
-            
-            boarding = min(4, waiting)
-       
+def maxCoins(piles):
+    piles.sort()
+    total = 0
+    n = len(piles) // 3
+    for i in range(n):
+        total += piles[len(piles) - 2 * (i + 1)]
+    return total
