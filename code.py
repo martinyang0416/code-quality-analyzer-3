056a8b3ea0aba428683ad6ebf71b4c2dba2e8c34
Analyzer <analyzer@test.com>
2026-02-09 13:44:06 -0800

    Update

--- code.py
+++ code.py
@@ -1,24 +1,11 @@
-from collections import deque
-
-s = input().strip()
-n = len(s)
-if n == 0:
-    print(0)
-    exit()
-
-prev = [i - 1 for i in range(n)]
-next = [i + 1 if i < n - 1 else -1 for i in range(n)]
-current_queue = deque()
-
-for i in range(n):
-    left = prev[i]
-    right = next[i]
-    has_diff = False
-    if left != -1 and s[left] != s[i]:
-        has_diff = True
-    if not has_diff and right != -1 and s[right] != s[i]:
-        has_diff = True
-    if has_diff:
-        current_queue.append(i)
-
-deleted = [False
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    first_chars = []
+    for _ in range(n):
+        city = input().strip()
+        first_chars.append(city[0])
+    if len(first_chars) == len(set(first_chars)):
+        print("YES")
+    else:
+        print("NO")
