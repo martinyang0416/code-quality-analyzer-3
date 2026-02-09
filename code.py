5a5379ceaecc03e18603f28c3cf3fd8336528e43
Analyzer <analyzer@test.com>
2026-02-09 14:47:04 -0800

    Update

--- code.py
+++ code.py
@@ -1,13 +1,10 @@
-def winnerSquareGame(n):
-    max_square_root = int(n ** 0.5)
-    squares = [i*i for i in range(max_square_root, 0, -1)]
-    dp = [False] * (n + 1)
-    dp[0] = False
-    for i in range(1, n + 1):
-        for s in squares:
-            if s > i:
-                continue
-            if not dp[i - s]:
-                dp[i] = True
-                break
-    return dp[n]
+def smallest_repunit_divisible_by_k(K):
+    if K % 2 == 0 or K % 5 == 0:
+        return -1
+    mod_val = 9 * K
+    current = 1
+    for m in range(1, mod_val + 1):
+        current = (current * 10) % mod_val
+        if current == 1:
+            return m
+    return -1
