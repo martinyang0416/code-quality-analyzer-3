62d12efc8f74473ecfc7796e01a7197eec00d60a
Analyzer <analyzer@test.com>
2026-02-09 14:50:48 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,17 @@
-def catMouseGame(graph):
-    memo = {}
-
-    def dfs(m, c, turn):
-        key = (m, c, turn)
-        if key in memo:
-            return memo[key]
-        
-        if m == 0:
-            memo[key] = 1
-            return 1
-        if m == c:
-            memo[key] = 2
-            return 2
-        
-        if turn == 1:
-            next_positions = [pos for pos in graph[c] if pos != 0]
-            if not next_positions:
-                memo[key] = 0
-                return 0
-        
-        memo[key]
+def lastSubstring(s: str) -> str:
+    n = len(s)
+    best = 0
+    for i in range(1, n):
+        current_length = 0
+        while True:
+            if best + current_length >= n or i + current_length >= n:
+                break
+            if s[best + current_length] != s[i + current_length]:
+                break
+            current_length += 1
+        if i + current_length >= n:
+            continue
+        if best + current_length >= n:
+            best = i
+            continue
+        if s[i 
