498747991cba553f954ae2fa885c77e95e310186
Analyzer <analyzer@test.com>
2026-02-09 14:49:23 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,14 @@
-from collections import deque
-from typing import List
-
-class Solution:
-    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        word_set = set(wordList)
-        if endWord not in word_set:
-            return 0
-        
-        queue = deque([(beginWord, 1)])
-        # Remove beginWord from the set if present to avoid revisiting
-        if beginWord in word_set:
-            word_set.remove(beginWord)
-        
-        while queue:
-            current_word, leve
+def wordBreak(s, wordDict):
+    word_set = set(wordDict)
+    max_length = max(len(word) for word in word_set) if word_set else 0
+    n = len(s)
+    dp = [False] * (n + 1)
+    dp[0] = True  # Empty string can be segmented
+    
+    for i in range(1, n + 1):
+        start = max(0, i - max_length)
+        for j in range(start, i):
+            if dp[j] and s[j:i] in word_set:
+                dp[i] = True
+                break  # No need to check further once found
+    return dp[n]
