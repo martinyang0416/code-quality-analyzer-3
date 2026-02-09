aec8b2f3b0b48e5d893666832bf7f37d946c0af7
Analyzer <analyzer@test.com>
2026-02-09 13:59:21 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,21 @@
-def compute_sum(a, b, c):
-    if b < c:
-        n = b - a + 1
-        first = c - b
-        last = c - a
-        return (first + last) * n // 2
-    elif a > c:
-        n = b - a + 1
-        first = a - c
-        last = b - c
-        return (first + last) * n // 2
-    else:
-        left_part = c - a
-        left_sum = left_part * (left_part + 1) // 2
-        right_part = b - c
-        right_sum = right_part * (right_part + 1) // 2
-        return left_sum + right_sum
+import sys
+from collections import deque
 
-n, K = map(int, input().split
+class Edge:
+    def __init__(self, to, rev, cap):
+        self.to = to
+        self.rev = rev
+        self.cap = cap
+
+class Dinic:
+    def __init__(self, n):
+        self.size = n
+        self.graph = [[] for _ in range(n)]
+    
+    def add_edge(self, fr, to, cap):
+        forward = Edge(to, len(self.graph[to]), cap)
+        backward = Edge(fr, len(self.graph[fr]), 0)
+        self.graph[fr].append(forward)
+        self.graph[to].append(backward)
+    
+   
