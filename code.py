43281ea944ef9321e7186fd55cf9bbce4795143b
Analyzer <analyzer@test.com>
2026-02-09 13:48:11 -0800

    Update

--- code.py
+++ code.py
@@ -1,24 +1,6 @@
-import heapq
-
-h, w, K = map(int, input().split())
-grid = []
-s_pos = None
-e_pos = None
-a_positions = []
-for i in range(h):
-    row = list(input().strip().replace(' ', ''))
-    for j in range(w):
-        if row[j] == 's':
-            s_pos = (i, j)
-        elif row[j] == 'e':
-            e_pos = (i, j)
-        elif row[j] == 'a':
-            a_positions.append((i, j))
-    grid.append(row)
-
-m = len(a_positions)
-if m < K:
-    print(-1)
-    exit()
-
-a_index = {pos: idx for idx, pos in enumerate(a_posi
+T = int(input())
+for _ in range(T):
+    line = input().strip()
+    words = line.split()
+    reversed_words = [word[::-1].lower() for word in words]
+    print(' '.join(reversed_words))
