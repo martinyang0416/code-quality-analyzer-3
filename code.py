506a7d5d78c17b1e1f10eb15392d511fc841b9fa
Analyzer <analyzer@test.com>
2026-02-09 13:03:44 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,4 @@
-n = int(input())
-p = list(map(int, input().split()))
-
-pos = [0] * (n + 1)  # 1-based indexing for values
-for i in range(n):
-    val = p[i]
-    pos[val] = i  # 0-based index
-
-class FenwickTree:
-    def __init__(self, size):
-        self.n = size
-        self.tree = [0] * (self.n + 2)  # 1-based indexing
-
-    def update(self, idx):
-        while idx <= self.n:
-            self.tree[idx] += 1
-            idx += idx & -idx
-
-    def query(self, idx):
-        res = 0
-        while idx > 0:
-           
+n, x, y = map(int, input().split())
+required = (y * n + 99) // 100
+clones = max(required - x, 0)
+print(clones)
