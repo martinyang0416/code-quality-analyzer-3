64edc86c58bd16db24588c234ec882855676de8f
Analyzer <analyzer@test.com>
2026-02-09 13:35:44 -0800

    Update

--- code.py
+++ code.py
@@ -1,27 +1,13 @@
-import math
+# Read the grid
+grid = [list(input().strip()) for _ in range(4)]
 
-n, px, py = map(int, input().split())
-
-vertices = []
-dists = []
-
-for _ in range(n):
-    x, y = map(int, input().split())
-    dx = x - px
-    dy = y - py
-    dist = math.hypot(dx, dy)
-    vertices.append((x, y))
-    dists.append(dist)
-
-min_dist = min(dists)
-max_dist = max(dists)
-
-for i in range(n):
-    a = vertices[i]
-    b = vertices[(i + 1) % n]
-    ax, ay = a
-    bx, by = b
-    dx_edge = bx - ax
-    dy_edge = by - ay
-    len_ab_sq = dx_edge ** 2 + dy_edge ** 2
-    if len_ab_sq == 
+# Precompute all possible triplets where three consecutive cells can form a line
+triplets = [
+    # Horizontal triplets
+    [(0,0), (0,1), (0,2)], [(0,1), (0,2), (0,3)],
+    [(1,0), (1,1), (1,2)], [(1,1), (1,2), (1,3)],
+    [(2,0), (2,1), (2,2)], [(2,1), (2,2), (2,3)],
+    [(3,0), (3,1), (3,2)], [(3,1), (3,2), (3,3)],
+    # Vertical triplets
+    [(0,0), (1,0), (2,0)], [(1,0), (2,0), (3,0)],
+    [(0,1), (1,1), (2,1)], [(1,1), (2,1)
