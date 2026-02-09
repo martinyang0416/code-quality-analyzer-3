5a71eb0e6bc4f9a078eedd770c39388a272f413e
Analyzer <analyzer@test.com>
2026-02-09 11:09:16 -0800

    Update

--- code.py
+++ code.py
@@ -1,2 +1,8 @@
-a = int(input())
-print(1 if a % 2 != 0 else 0)
+# Read input values
+N, M = map(int, input().split())
+
+# Check if N is less than or equal to M*(M-1)
+if N <= M * (M - 1):
+    print("YES")
+else:
+    print("NO")
