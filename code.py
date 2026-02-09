e2607a658e537e3fc445ef29a7c2275bf4ef31f2
Analyzer <analyzer@test.com>
2026-02-09 13:22:55 -0800

    Update

--- code.py
+++ code.py
@@ -1,2 +1,21 @@
-a, b = map(int, input().split())
-print(a * b if 1 <= a <= 9 and 1 <= b <= 9 else -1)
+import sys
+
+def is_convex(points):
+    cross = []
+    for i in range(4):
+        p = points[i]
+        q = points[(i+1)%4]
+        r = points[(i+2)%4]
+        dx1 = q[0] - p[0]
+        dy1 = q[1] - p[1]
+        dx2 = r[0] - q[0]
+        dy2 = r[1] - q[1]
+        cross_val = dx1 * dy2 - dy1 * dx2
+        cross.append(cross_val)
+    if all(c >= 0 for c in cross) or all(c <= 0 for c in cross):
+        return "YES"
+    else:
+        return "NO"
+
+for line in sys.stdin:
+    data = list(map(float, line
