043b3d937fa780273733627fc43d2124e82475c7
Analyzer <analyzer@test.com>
2026-02-09 10:18:57 -0800

    Update

--- code.py
+++ code.py
@@ -1,29 +1,13 @@
-t = int(input())
-test_cases = []
-max_N = 0
-for _ in range(t):
-    n = int(input())
-    test_cases.append(n)
-    if n > max_N:
-        max_N = n
+n = int(input())
+prefs = [list(map(int, input().split())) for _ in range(n)]
+gift = [0] * (n + 1)
+used = set()
 
-k_max = max_N - 1
+for i in range(1, n + 1):
+    for j in prefs[i - 1]:
+        if j != i and j not in used:
+            gift[i] = j
+            used.add(j)
+            break
 
-if k_max < 0:
-    dp = [0]
-else:
-    dp = [0] * (k_max + 1)
-    dp[0] = 1
-    if k_max >= 1:
-        dp[1] = 1
-    if k_max >= 2:
-        dp[2] = 1
-    for k in range(3, k_max + 1):
-        dp[k] = dp[k - 1] + dp[k - 3]
-
-for n in test_cases:
-    k = n - 1
-    if k < 0:
-        print(0)
-    else:
-        print(dp[k])
+print(' '.join(map(str, gift[1:])))
