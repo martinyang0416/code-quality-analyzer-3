7dc5c970030b95164cef7854475839340a8e55c6
Analyzer <analyzer@test.com>
2026-02-09 14:46:23 -0800

    Update

--- code.py
+++ code.py
@@ -1,13 +1,13 @@
-import math
-
-def kthFactor(n: int, k: int) -> int:
-    lower = []
-    higher = []
-    for i in range(1, math.isqrt(n) + 1):
-        if n % i == 0:
-            lower.append(i)
-            j = n // i
-            if j != i:
-                higher.append(j)
-    factors = lower + higher[::-1]
-    return factors[k-1] if len(factors) >= k else -1
+def winnerSquareGame(n):
+    max_square_root = int(n ** 0.5)
+    squares = [i*i for i in range(max_square_root, 0, -1)]
+    dp = [False] * (n + 1)
+    dp[0] = False
+    for i in range(1, n + 1):
+        for s in squares:
+            if s > i:
+                continue
+            if not dp[i - s]:
+                dp[i] = True
+                break
+    return dp[n]
