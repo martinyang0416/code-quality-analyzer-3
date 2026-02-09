1d958e2b464044ffe79da1d2b5b4e2916287cff3
Analyzer <analyzer@test.com>
2026-02-09 11:26:28 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,23 @@
 def putaway(A, B, T, X, Y, W, S):
-    if T != 2 or (A + B) != 2:
-        return -1  # according to problem constraints, but code is for subcase
-    
-    # Check each toy can be handled by at least one robot
-    for i in range(2):
-        can_weak = False
-        can_small = False
-        if A > 0:
-            for x in X:
-                if W[i] < x:
-                    can_weak = True
-                    break
-        if B > 0:
-            for y in Y:
-                if S[i] < y:
-               
+    # Check if each toy can be assigned to at least one robot
+    can0 = False
+    can1 = False
+
+    # Check for toy 0
+    if A > 0:
+        for x in X:
+            if W[0] < x:
+                can0 = True
+                break
+    if not can0 and B > 0:
+        for y in Y:
+            if S[0] < y:
+                can0 = True
+                break
+    if not can0:
+        return -1
+
+    # Check for toy 1
+    if A > 0:
+        for x in X:
+            if W[1] < x:
