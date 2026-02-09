c7edce4ee24af883269a6922b1ee2a36de99043d
Analyzer <analyzer@test.com>
2026-02-09 15:13:36 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,22 @@
 import sys
 
-def is_grid_stable(R, C, grid):
-    for i in range(R):
-        for j in range(C):
-            neighbors = 4
-            if i == 0 or i == R - 1:
-                neighbors -= 1
-            if j == 0 or j == C - 1:
-                neighbors -= 1
-            if neighbors <= grid[i][j]:
-                return False
-    return True
-
 def main():
     input = sys.stdin.read().split()
     ptr = 0
     T = int(input[ptr])
     ptr += 1
     for _ in range(T):
-        R = int(input[ptr])
-        C
+        n, k = map(int, input[ptr:ptr+2])
+        ptr += 2
+        Q = list(map(int, input[ptr:ptr+n]))
+        ptr += n
+        carry = 0
+        found = False
+        for i in range(n):
+            total = carry + Q[i]
+            if total < k:
+                print(i + 1)
+                found = True
+                break
+            carry = total - k
+        if
