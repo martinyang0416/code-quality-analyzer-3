d18c4292d8d3ee5cd2dd0ef99dea3b58111de3f3
Analyzer <analyzer@test.com>
2026-02-09 15:09:21 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,8 @@
-import bisect
-import sys
-
-def main():
-    input = sys.stdin.read().split()
-    ptr = 0
-    T = int(input[ptr])
-    ptr += 1
-    for _ in range(T):
-        N, Q = map(int, input[ptr:ptr+2])
-        ptr += 2
-        A = list(map(int, input[ptr:ptr+N]))
-        ptr += N
-        sorted_A = sorted(A)
-        element_to_pos = {val: i+1 for i, val in enumerate(A)}  # 1-based
-        
-        # Precompute sorted list for S_l and S_g
-        sorted_values = sorted_A
-        
-        for __ in range(Q):
- 
+# Read the number of test cases
+T = int(input())
+for _ in range(T):
+    n, m = map(int, input().split())
+    if n == 1:
+        print(0)
+    else:
+        print(m * (n - 1))
