fad5518d7d0b228eae420fb4cd373066fe593807
Analyzer <analyzer@test.com>
2026-02-09 10:20:51 -0800

    Update

--- code.py
+++ code.py
@@ -1,4 +1,13 @@
-n, m = map(int, input().split())
+n = int(input())
 s = list(map(int, input().split()))
-s.sort()
-print(sum(s))
+swaps = []
+for i in range(n):
+    for j in range(n - i - 1):
+        if s[j] > s[j + 1]:
+            # Swap the elements
+            s[j], s[j + 1] = s[j + 1], s[j]
+            # Record the swap using 1-based indices
+            swaps.append((j + 1, j + 2))
+# Print each swap
+for swap in swaps:
+    print(swap[0], swap[1])
