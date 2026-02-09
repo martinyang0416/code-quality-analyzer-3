8bf9042f9a8316551569916bd5a80455d329a9aa
Analyzer <analyzer@test.com>
2026-02-09 14:08:43 -0800

    Update

--- code.py
+++ code.py
@@ -1,7 +1,14 @@
-n, k = map(int, input().split())
-a = list(map(int, input().split()))
-total = 0
-for ai in a:
-    t = min(3, ai // k)
-    total += ai - t * k
-print(total)
+n, m, q = map(int, input().split())
+count1 = 0  # even i and even j
+count2 = 0  # odd i and odd j
+
+for _ in range(q):
+    i, j = map(int, input().split())
+    if i % 2 == 0 and j % 2 == 0:
+        count1 += 1
+    else:
+        count2 += 1
+    if count1 == 0 or count2 == 0:
+        print("YES")
+    else:
+        print("NO")
