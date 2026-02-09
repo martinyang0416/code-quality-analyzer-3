63e674d35908aae5e6b6d28fa2d619f8f77a7af1
Analyzer <analyzer@test.com>
2026-02-09 10:08:15 -0800

    Update

--- code.py
+++ code.py
@@ -1,11 +1,20 @@
-b = int(input())
-m = input().strip()
+n = int(input())
+scores = list(map(int, input().split()))
+scores.sort()
 
-if m == '0':
-    print(0)
-else:
-    res = 0
-    for c in m:
-        d = int(c)
-        res = res * b + d
-    print(res)
+max_rating = 0
+for R in range(n, 0, -1):
+    # Binary search to find the first index where score >= R
+    left, right = 0, n
+    while left < right:
+        mid = (left + right) // 2
+        if scores[mid] >= R:
+            right = mid
+        else:
+            left = mid + 1
+    count = n - left
+    if count >= R:
+        max_rating = R
+        break
+
+print(max_rating)
