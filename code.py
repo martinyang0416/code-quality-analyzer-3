db8aa3729ec00626690666b80308fb7747a182f2
Analyzer <analyzer@test.com>
2026-02-09 10:36:31 -0800

    Update

--- code.py
+++ code.py
@@ -1,11 +1,27 @@
-k = int(input())
-for i in range(1, k):
-    row = []
-    for j in range(1, k):
-        if j == 0:
-            row.append("N/A")
-        elif i % j == 0:
-            row.append(str(i // j))
-        else:
-            row.append("N/A")
-    print(' '.join(row))
+import bisect
+
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    
+    idx = 0
+    n = int(data[idx])
+    k = int(data[idx+1])
+    q = int(data[idx+2])
+    idx +=3
+    
+    MAX = 200000
+    diff = [0] * (MAX + 2)  # 0 to MAX+1
+    
+    for _ in range(n):
+        l = int(data[idx])
+        r = int(data[idx+1])
+        idx +=2
+        diff[l] += 1
+        if r + 1 <= MAX:
+            diff[r + 1] -= 1
+    
+    current = 0
+    freq = [0] * (MAX + 1)  # 1 to MAX
+    
