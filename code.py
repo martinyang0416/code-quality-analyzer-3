1bb17175246becb41d57a32cb098a83afeeb89c8
Analyzer <analyzer@test.com>
2026-02-09 10:16:31 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,2 @@
-n = int(input())
-degrees = [0] * (n + 1)
-for _ in range(n - 1):
-    u, v = map(int, input().split())
-    degrees[u] += 1
-    degrees[v] += 1
-
-s = 0
-for i in range(1, n + 1):
-    d = degrees[i]
-    s += d * (d - 1) // 2
-
-total = (n - 1) + s
-print(total)
+M = int(input())
+print('1 ' * M)
