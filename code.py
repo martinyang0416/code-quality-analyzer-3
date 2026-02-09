035bd7cdb25da7d4d1e6e972b0a5022767ec966a
Analyzer <analyzer@test.com>
2026-02-09 14:41:09 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,18 @@
-def split_array(nums, m):
-    left = max(nums)
-    right = sum(nums)
-    
-    while left < right:
-        mid = (left + right) // 2
-        current_sum = 0
-        splits = 1
-        
-        for num in nums:
-            if current_sum + num > mid:
-                splits += 1
-                current_sum = num
-            else:
-                current_sum += num
-        
-        if splits <= m:
-            right = mid
-        else:
-            left = mid + 1
-    
-    return left
+from collections import defaultdict
+
+def subarraysWithKDistinct(A, K):
+    def atMost(k):
+        freq = defaultdict(int)
+        left = 0
+        count = 0
+        res = 0
+        for right in range(len(A)):
+            if freq[A[right]] == 0:
+                count += 1
+            freq[A[right]] += 1
+            while count > k:
+                freq[A[left]] -= 1
+                if freq[A[left]] == 0:
+                    count -= 1
+                left += 1
+            res += right - left + 1
