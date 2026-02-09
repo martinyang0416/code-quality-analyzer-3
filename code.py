289b17242d4e11c0b3b634df180821c4299c077a
Analyzer <analyzer@test.com>
2026-02-09 10:38:48 -0800

    Update

--- code.py
+++ code.py
@@ -1,13 +1,26 @@
-def find_mth_number(M):
-    if M == 0:
-        return 0
-    digits = []
-    while M > 0:
-        rem = M % 7
-        digits.append(str(rem))
-        M = M // 7
-    digits.reverse()
-    return int(''.join(digits))
+def factorize(m):
+    factors = {}
+    i = 2
+    while i * i <= m:
+        while m % i == 0:
+            factors[i] = factors.get(i, 0) + 1
+            m //= i
+        i += 1
+    if m > 1:
+        factors[m] = 1
+    return factors
 
-M = int(input())
-print(find_mth_number(M))
+def solve():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    n = int(data[0])
+    m = int(data[1])
+    a = list(map(int, data[2:2+n]))
+    
+    factors = factorize(m)
+    if not factors:
+        print("YES")
+        return
+    
+    primes = li
